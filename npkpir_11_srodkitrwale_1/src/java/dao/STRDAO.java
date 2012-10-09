91f1b24f9d9e7828175f3ae5331302ae675fd31f
Brzaskun <Brzaskun@gmail.com>
2012-10-09 18:03:51 +0000

    podatkowa ksiega nowy wiersz

--- npkpir_11_srodkitrwale_1/src/java/dao/STRDAO.java
+++ npkpir_11_srodkitrwale_1/src/java/dao/STRDAO.java
@@ -7,8 +7,14 @@ package dao;
 import entity.SrodekTrw;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+import session.STRFacade;
 
 /**
  *
@@ -16,22 +22,80 @@ import javax.inject.Named;
  */
 @Named
 public class STRDAO implements Serializable{
-    private List<SrodekTrw> EwidencjaSrodkowTrwalych;
+   
+    @Inject
+    private STRFacade strFacade;
+    //tablica wciagnieta z bazy danych
+    
+    private List<SrodekTrw> downloadedSTR;
 
     public STRDAO() {
-        EwidencjaSrodkowTrwalych = new ArrayList<SrodekTrw>();
+        downloadedSTR = new ArrayList<SrodekTrw>();
     }
     
     
 
-    public List<SrodekTrw> getEwidencjaSrodkowTrwalych() {
-        return EwidencjaSrodkowTrwalych;
+    public List<SrodekTrw> getdownloadedSTR() {
+        return downloadedSTR;
     }
 
-    public void setEwidencjaSrodkowTrwalych(List<SrodekTrw> EwidencjaSrodkowTrwalych) {
-        this.EwidencjaSrodkowTrwalych = EwidencjaSrodkowTrwalych;
+    public void setdownloadedSTR(List<SrodekTrw> downloadedSTR) {
+        this.downloadedSTR = downloadedSTR;
     }
 
+    @PostConstruct
+    public void init(){
+        Collection c = null;
+        try {
+            c = strFacade.findAll();
+        } catch (Exception e) {
+            System.out.println("Blad w pobieraniu z bazy danych. Spradzic czy nie pusta, iniekcja oraz  lacze z baza dziala" + e.toString());
+        }
+        if(c.size()>0){
+        downloadedSTR.addAll(c);
+        System.out.println("Pobrano z bazy danych." + c.toString());
+        }
+    }
+           
+   
+//    
+//    public SrodekTrw znajdzDuplikat(SrodekTrw selD) throws Exception{
+//        SrodekTrw tmp = null;
+//        tmp = strFacade.duplicat(selD);
+//        return tmp;
+//        }
+   
+    
+     public void dodajNowyWpis(SrodekTrw selectedSTR){
+        try {
+            strFacade.create(selectedSTR);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Nowy strument zachowany", selectedSTR.getNazwa().toString());
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        } catch (Exception e) {
+            System.out.println("Nie utworzono srodka "+e.toString());
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,"SrodekTrwument nie uwtorzony DAO"+e.toString(),"");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            
+        }
+    }
+    
+    public void destroy(SrodekTrw selectedSTR) {
+        try {
+        strFacade.remove(selectedSTR);
+        } catch (Exception e) {
+            System.out.println("Nie usnieto "+selectedSTR.getNazwa()+" "+e.toString());
+        }
+    }
+        
+     
+    public void edit(SrodekTrw selectedSTR){
+        try {
+            strFacade.edit(selectedSTR);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace().toString());
+        }
+     }
+    
     
     
     
--- npkpir_11_srodkitrwale_1/src/java/view/STRView.java
+++ npkpir_11_srodkitrwale_1/src/java/view/STRView.java
@@ -9,13 +9,10 @@ import entity.SrodekTrw;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Map;
-import javax.faces.application.Application;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
 import javax.inject.Inject;
 import org.primefaces.context.RequestContext;
 
@@ -35,7 +32,15 @@ public class STRView implements Serializable{
     
     private boolean pokazSTR;
     
-    
+    private boolean umorzeniaBiezace;
+
+    public boolean isUmorzeniaBiezace() {
+        return umorzeniaBiezace;
+    }
+
+    public void setUmorzeniaBiezace(boolean umorzeniaBiezace) {
+        this.umorzeniaBiezace = umorzeniaBiezace;
+    }
     
     public STRView() {
         selectedSTR = new SrodekTrw();
@@ -78,34 +83,49 @@ public class STRView implements Serializable{
     
     public void dodajSrodekTrwaly(SrodekTrw STR){
         try { 
-        Double netto = STR.getWartoscPoczatkowa();
-        Double stawkaamortyzacji = STR.getStawkaAmortyzacji();
+        Double netto = STR.getNetto();
+        Double stawkaamortyzacji = STR.getStawka();
         BigDecimal tmp1 = BigDecimal.valueOf(stawkaamortyzacji);
         tmp1 = tmp1.setScale(2, RoundingMode.HALF_EVEN);
         tmp1 = tmp1.multiply(BigDecimal.valueOf(netto));
         tmp1 = tmp1.divide(BigDecimal.valueOf(100));
         tmp1 = tmp1.setScale(2, RoundingMode.HALF_EVEN);
-        BigDecimal tmp2 = tmp1.divide(BigDecimal.valueOf(12));
+        BigDecimal tmp2 = tmp1.divide(BigDecimal.valueOf(12),2, RoundingMode.HALF_EVEN);
         tmp2 = tmp2.setScale(2, RoundingMode.HALF_EVEN);
         Double odpisrok = Double.parseDouble(tmp1.toString());
         Double odpismiesiac = Double.parseDouble(tmp2.toString());
-        STR.setRocznaKwotaOdpisu(odpisrok);
-        STR.setMiesiecznaKwotaOdpisu(odpismiesiac);
-        sTRDAO.getEwidencjaSrodkowTrwalych().add(STR);
+        STR.setOdpisrok(odpisrok);
+        STR.setOdpismc(odpismiesiac);
+        sTRDAO.dodajNowyWpis(STR);
         setSelectedSTR(STR);
         RequestContext ctx = null;
         ctx.getCurrentInstance().update("srodki:panelekXA");
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Nowy srodek zachowany", selectedSTR.getNazwaSrodka());
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Nowy srodek zachowany", selectedSTR.getNazwa());
         FacesContext.getCurrentInstance().addMessage(null, msg);
+//        if(umorzeniaBiezace==true){
+//            Double odpis = STR.getMiesiecznaKwotaOdpisu();
+//        }
         } catch (Exception e) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Nowy srodek nie zachowany", selectedSTR.getNazwaSrodka());
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Nowy srodek nie zachowany", selectedSTR.getNazwa());
         FacesContext.getCurrentInstance().addMessage(null, msg);
         }
    }
     
     public int ile(){
-        return sTRDAO.getEwidencjaSrodkowTrwalych().size();
+        return sTRDAO.getdownloadedSTR().size();
     }
     
-   
+    public static void main(String [] args){
+        Double netto = 150.0;
+        Double stawkaamortyzacji = 20.0;
+        BigDecimal tmp1 = BigDecimal.valueOf(stawkaamortyzacji);
+        tmp1 = tmp1.setScale(2, RoundingMode.HALF_EVEN);
+        tmp1 = tmp1.multiply(BigDecimal.valueOf(netto));
+        tmp1 = tmp1.divide(BigDecimal.valueOf(100));
+        tmp1 = tmp1.setScale(2, RoundingMode.HALF_EVEN);
+        BigDecimal tmp2 = tmp1.divide(BigDecimal.valueOf(12),2, RoundingMode.HALF_EVEN);
+        tmp2 = tmp2.setScale(2, RoundingMode.HALF_EVEN);
+        Double odpisrok = Double.parseDouble(tmp1.toString());
+        Double odpismiesiac = Double.parseDouble(tmp2.toString());
+   }
 }
