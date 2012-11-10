c84c8d4d8dba09d954c146210e9e5a0199b94ce0
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2012-11-10 00:02:27 +0000

    zus podwojny klucz zrobiony

--- trunk/npkpir_16_podatki_a/src/java/embeddable/STRtabela.java
+++ trunk/npkpir_16_podatki_a/src/java/embeddable/STRtabela.java
@@ -5,7 +5,6 @@
 package embeddable;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
  *
--- trunk/npkpir_16_podatki_a/src/java/entity/Zusstawki.java
+++ trunk/npkpir_16_podatki_a/src/java/entity/Zusstawki.java
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Zusstawki.findAll", query = "SELECT z FROM Zusstawki z"),
     @NamedQuery(name = "Zusstawki.findByRok", query = "SELECT z FROM Zusstawki z WHERE z.zusstawkiPK.rok = :rok"),
     @NamedQuery(name = "Zusstawki.findByMiesiac", query = "SELECT z FROM Zusstawki z WHERE z.zusstawkiPK.miesiac = :miesiac"),
-    @NamedQuery(name = "Zusstawki.findByZus51", query = "SELECT z FROM Zusstawki z WHERE z.zus51 = :zus51"),
+    @NamedQuery(name = "Zusstawki.findByZus51", query = "SELECT z FROM Zusstawki z WHERE z.zus51ch = :zus51ch"),
     @NamedQuery(name = "Zusstawki.findByZus52", query = "SELECT z FROM Zusstawki z WHERE z.zus52 = :zus52"),
     @NamedQuery(name = "Zusstawki.findByZus53", query = "SELECT z FROM Zusstawki z WHERE z.zus53 = :zus53")})
 public class Zusstawki implements Serializable {
@@ -33,8 +33,10 @@ public class Zusstawki implements Serializable {
     @EmbeddedId
     protected ZusstawkiPK zusstawkiPK;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-    @Column(name = "zus51")
-    private Double zus51;
+    @Column(name = "zus51ch")
+    private Double zus51ch;
+    @Column(name = "zus51bch")
+    private Double zus51bch;
     @Column(name = "zus52")
     private Double zus52;
     @Column(name = "zus53")
@@ -64,13 +66,23 @@ public class Zusstawki implements Serializable {
         this.zusstawkiPK = zusstawkiPK;
     }
 
-    public Double getZus51() {
-        return zus51;
+    public Double getZus51ch() {
+        return zus51ch;
     }
 
-    public void setZus51(Double zus51) {
-        this.zus51 = zus51;
+    public void setZus51ch(Double zus51ch) {
+        this.zus51ch = zus51ch;
     }
+
+    public Double getZus51bch() {
+        return zus51bch;
+    }
+
+    public void setZus51bch(Double zus51bch) {
+        this.zus51bch = zus51bch;
+    }
+
+   
 
     public Double getZus52() {
         return zus52;
