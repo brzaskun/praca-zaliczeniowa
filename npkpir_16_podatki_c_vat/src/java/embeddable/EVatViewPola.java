3a15072f669d45907242e462e8b1448fc68ce624
Brzaskun <Brzaskun@gmail.com>
2012-11-11 21:02:26 +0000

    rozpoczynamy vat

--- npkpir_16_podatki_c_vat/src/java/embeddable/EVatViewPola.java
+++ npkpir_16_podatki_c_vat/src/java/embeddable/EVatViewPola.java
@@ -4,25 +4,69 @@
  */
 package embeddable;
 
+import java.io.Serializable;
 import javax.inject.Named;
+import javax.persistence.Embeddable;
 
 /**
  *
  * @author Osito
  */
-@Named(value="EVatViewPola")
-public class EVatViewPola {
+@Named
+@Embeddable
+public class EVatViewPola implements Serializable{
+    private int id;
+    private String dataWyst;
+    private String dataSprz;
+    private String nrWlDk;
+    private Kl kontr;
     private String opis;
     private double netto;
     private double vat;
+    private String opizw;
+    private String nazwaewidencji;
 
     public EVatViewPola() {
     }
 
-    public EVatViewPola(String opis, double netto, double vat) {
-        this.opis = opis;
-        this.netto = netto;
-        this.vat = vat;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDataWyst() {
+        return dataWyst;
+    }
+
+    public void setDataWyst(String dataWyst) {
+        this.dataWyst = dataWyst;
+    }
+
+    public String getDataSprz() {
+        return dataSprz;
+    }
+
+    public void setDataSprz(String dataSprz) {
+        this.dataSprz = dataSprz;
+    }
+
+    public String getNrWlDk() {
+        return nrWlDk;
+    }
+
+    public void setNrWlDk(String nrWlDk) {
+        this.nrWlDk = nrWlDk;
+    }
+
+    public Kl getKontr() {
+        return kontr;
+    }
+
+    public void setKontr(Kl kontr) {
+        this.kontr = kontr;
     }
 
     public String getOpis() {
@@ -48,7 +92,24 @@ public class EVatViewPola {
     public void setVat(double vat) {
         this.vat = vat;
     }
-    
+
+    public String getOpizw() {
+        return opizw;
+    }
+
+    public void setOpizw(String opizw) {
+        this.opizw = opizw;
+    }
+
+    public String getNazwaewidencji() {
+        return nazwaewidencji;
+    }
+
+    public void setNazwaewidencji(String nazwaewidencji) {
+        this.nazwaewidencji = nazwaewidencji;
+    }
+
+   
     
     
 }
