3cc0953dc86bc9f8c152cdb15258338d1b334a6c
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2012-12-11 22:09:02 +0000

    wzorce

---
+++ trunk/npkpir_17_R_1/src/java/dao/DokDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.Dok;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import session.SessionFacade;
+
+/**
+ *
+ * @author Osito
+ */
+@Named(value = "DokDAO")
+public class DokDAO extends DAO implements Serializable {
+
+    @Inject
+    private SessionFacade dokFacade;
+    //tablica wciagnieta z bazy danych
+
+    public DokDAO() {
+        super(Dok.class);
+    }
+
+
+    public Dok znajdzDuplikat(Dok selD) throws Exception {
+        Dok tmp = null;
+        tmp = dokFacade.dokumentDuplicat(selD);
+        return tmp;
+    }
+
+//    
+//     public Dok znajdzPoprzednika(Integer rok, Integer mc) throws Exception{
+//        Dok tmp = null;
+//        tmp = dokFacade.poprzednik(rok,mc);
+//        return tmp;
+//        }
+
+    public List<Dok> zwrocBiezacegoKlienta(String pod) {
+        List<Dok> lista = new ArrayList<>();
+        Iterator it;
+        it = downloaded.iterator();
+        while (it.hasNext()) {
+            Dok tmp = (Dok) it.next();
+            if (tmp.getPodatnik().equals(pod)) {
+                lista.add(tmp);
+            }
+        }
+        return lista;
+    }
+
+    public List<Dok> zwrocBiezacegoKlientaRok(String pod, Integer rok) {
+        List<Dok> lista = new ArrayList<>();
+        Iterator it;
+        it = downloaded.iterator();
+        while (it.hasNext()) {
+            Dok tmp = (Dok) it.next();
+            if (tmp.getPodatnik().equals(pod) && tmp.getPkpirR().equals(rok.toString())) {
+                lista.add(tmp);
+            }
+        }
+        return lista;
+    }
+    
+}
