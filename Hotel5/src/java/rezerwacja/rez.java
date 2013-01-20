10df54d9fa3d04e96d3dc51ca180c6f9c1bbda11
Brzaskun <Brzaskun@gmail.com>
2013-01-20 12:22:07 +0000

    hotel5

---
+++ Hotel5/src/java/rezerwacja/rez.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rezerwacja;
+
+import java.io.Serializable;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Osito
+ */
+@Named
+@Stateless
+public class rez implements Serializable {
+    
+    
+}
