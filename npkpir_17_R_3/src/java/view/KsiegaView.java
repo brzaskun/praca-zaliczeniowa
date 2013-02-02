9dbc2c72f19ec2061ed6cdf4a29fc07af248064d
Brzaskun <Brzaskun@gmail.com>
2013-02-02 21:05:37 +0000

    podsumowanie ksiegi

--- npkpir_17_R_3/src/java/view/KsiegaView.java
+++ npkpir_17_R_3/src/java/view/KsiegaView.java
@@ -6,12 +6,14 @@ package view;
 
 import embeddable.DokKsiega;
 import entity.Dok;
+import entity.Klienci;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.inject.Inject;
 
 /**
  *
@@ -21,8 +23,11 @@ import javax.faces.bean.ManagedProperty;
 public class KsiegaView {
     @ManagedProperty(value = "#{DokTabView}")
     private DokTabView dokTabView;
-    private ArrayList<DokKsiega> lista;
+    private static ArrayList<DokKsiega> lista;
     private DokKsiega selDokument;
+    @Inject private DokKsiega podsumowanie;
+    
+    
 
     public KsiegaView() {
         lista = new ArrayList<>();
@@ -31,10 +36,17 @@ public class KsiegaView {
     @PostConstruct
     private void init(){
         List<Dok> tmplist = dokTabView.getObiektDOKmrjsfSel();
-        Iterator it;
-        it = tmplist.iterator();
-        while (it.hasNext()){
-            Dok tmp = (Dok) it.next();
+        podsumowanie.setOpis("Podsumowanie");
+        podsumowanie.setKolumna7(0.0);
+        podsumowanie.setKolumna8(0.0);
+        podsumowanie.setKolumna9(0.0);
+        podsumowanie.setKolumna10(0.0);
+        podsumowanie.setKolumna11(0.0);
+        podsumowanie.setKolumna12(0.0);
+        podsumowanie.setKolumna13(0.0);
+        podsumowanie.setKolumna14(0.0);
+        podsumowanie.setKolumna15(0.0);
+        for (Dok tmp : tmplist){
             DokKsiega dk = new DokKsiega();
             dk.setIdDok(tmp.getIdDok());
             dk.setTypdokumentu(tmp.getTypdokumentu());
@@ -47,63 +59,87 @@ public class KsiegaView {
             switch(tmp.getPkpirKol()){
                 case "przych. sprz":
                     dk.setKolumna7(tmp.getKwota());
+                    podsumowanie.setKolumna7(podsumowanie.getKolumna7()+tmp.getKwota());
                     break;
                 case "pozost. przych.":
                     dk.setKolumna8(tmp.getKwota());
+                    podsumowanie.setKolumna8(podsumowanie.getKolumna8()+tmp.getKwota());
                     break;
                 case "zakup tow. i mat.":
                     dk.setKolumna10(tmp.getKwota());
+                    podsumowanie.setKolumna10(podsumowanie.getKolumna10()+tmp.getKwota());
                     break;
                 case "koszty ub.zak.":
                     dk.setKolumna11(tmp.getKwota());
+                    podsumowanie.setKolumna11(podsumowanie.getKolumna11()+tmp.getKwota());
                     break;
                 case "wynagrodzenia":
                     dk.setKolumna12(tmp.getKwota());
+                    podsumowanie.setKolumna12(podsumowanie.getKolumna12()+tmp.getKwota());
                     break;
                 case "poz. koszty":
                     dk.setKolumna13(tmp.getKwota());
+                    podsumowanie.setKolumna13(podsumowanie.getKolumna13()+tmp.getKwota());
                     break;
                  case "inwestycje":
                      dk.setKolumna15(tmp.getKwota());
+                     podsumowanie.setKolumna15(podsumowanie.getKolumna15()+tmp.getKwota());
                     break;   
             }
             if(tmp.getPkpirKolX()!=null){
             switch(tmp.getPkpirKolX()){
                 case "przych. sprz":
                     dk.setKolumna7(tmp.getKwotaX());
+                    podsumowanie.setKolumna7(podsumowanie.getKolumna7()+tmp.getKwotaX());
                     break;
                 case "pozost. przych.":
                     dk.setKolumna8(tmp.getKwotaX());
+                    podsumowanie.setKolumna8(podsumowanie.getKolumna8()+tmp.getKwotaX());
                     break;
                 case "zakup tow. i mat.":
                     dk.setKolumna10(tmp.getKwotaX());
+                    podsumowanie.setKolumna10(podsumowanie.getKolumna10()+tmp.getKwotaX());
                     break;
                 case "koszty ub.zak.":
                     dk.setKolumna11(tmp.getKwotaX());
+                    podsumowanie.setKolumna11(podsumowanie.getKolumna11()+tmp.getKwotaX());
                     break;
                 case "wynagrodzenia":
                     dk.setKolumna12(tmp.getKwotaX());
+                    podsumowanie.setKolumna12(podsumowanie.getKolumna12()+tmp.getKwotaX());
                     break;
                 case "poz. koszty":
                     dk.setKolumna13(tmp.getKwotaX());
+                    podsumowanie.setKolumna13(podsumowanie.getKolumna13()+tmp.getKwotaX());
                     break;
                  case "inwestycje":
                      dk.setKolumna15(tmp.getKwotaX());
+                     podsumowanie.setKolumna15(podsumowanie.getKolumna15()+tmp.getKwotaX());
                     break;   
             }}
             if(dk.getKolumna7()!=null&&dk.getKolumna8()!=null) {
                 dk.setKolumna9(dk.getKolumna7()+dk.getKolumna8());
+                podsumowanie.setKolumna9(podsumowanie.getKolumna9()+dk.getKolumna9());
             } else if (dk.getKolumna7()!=null) {
                 dk.setKolumna9(dk.getKolumna7());
+                podsumowanie.setKolumna9(podsumowanie.getKolumna9()+dk.getKolumna9());
             } else {
                 dk.setKolumna9(dk.getKolumna8());
+                try {
+                podsumowanie.setKolumna9(podsumowanie.getKolumna9()+dk.getKolumna9());
+                } catch (Exception e){}
             }
             if (dk.getKolumna12()!=null&&dk.getKolumna13()!=null){
                 dk.setKolumna14(dk.getKolumna12()+dk.getKolumna13());
+                podsumowanie.setKolumna14(podsumowanie.getKolumna14()+dk.getKolumna14());
             } else if (dk.getKolumna12()!=null){
                 dk.setKolumna14(dk.getKolumna12());
+                podsumowanie.setKolumna14(podsumowanie.getKolumna14()+dk.getKolumna14());
             } else {
                 dk.setKolumna14(dk.getKolumna13());
+                try{
+                podsumowanie.setKolumna14(podsumowanie.getKolumna14()+dk.getKolumna14());
+                } catch (Exception e){}
             }
             dk.setUwagi(tmp.getUwagi());
             dk.setPkpirM(tmp.getPkpirM());
@@ -115,8 +151,13 @@ public class KsiegaView {
             dk.setDokumentProsty(tmp.isDokumentProsty());
             lista.add(dk);
          }
+        podsumowanie.setIdDok(new Long(1222));
+        podsumowanie.setKontr(new Klienci());
+        lista.add(podsumowanie);
     }
 
+    
+    
     public ArrayList<DokKsiega> getLista() {
         return lista;
     }
