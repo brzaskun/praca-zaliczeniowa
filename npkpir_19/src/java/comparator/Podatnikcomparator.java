f56cd54af0a1cfab8b6d434ad6756f2e11e2abbb
Brzaskun <Brzaskun@gmail.com>
2013-02-20 20:07:10 +0000

    edycja podatnika, wzorce dokumenty przez edycje

--- npkpir_19/src/java/comparator/Podatnikcomparator.java
+++ npkpir_19/src/java/comparator/Podatnikcomparator.java
@@ -4,15 +4,8 @@
  */
 package comparator;
 
-import entity.Dok;
 import entity.Podatnik;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Comparator;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.inject.Named;
 
 /**
@@ -24,8 +17,8 @@ public class Podatnikcomparator implements Comparator<Podatnik> {
 
     @Override
     public int compare(Podatnik o1, Podatnik o2) {
-        String datao1 = o1.getNazwapelna();
-        String datao2 = o2.getNazwapelna();
+        String datao1 = o1.getNazwapelna().toLowerCase();
+        String datao2 = o2.getNazwapelna().toLowerCase();
         return datao1.compareTo(datao2);
     }
     
