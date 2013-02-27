cce68ffb4066f296f0c1f14b4e5060064f861906
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-02-27 19:05:11 +0000

    PIT SPÓŁKI

--- trunk/npkpir_19/src/java/view/PitView.java
+++ trunk/npkpir_19/src/java/view/PitView.java
@@ -5,7 +5,10 @@
 package view;
 
 import dao.PitDAO;
+import dao.PodatnikDAO;
+import embeddable.Udzialy;
 import entity.Pitpoz;
+import entity.Podatnik;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,8 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.inject.Inject;
+import msg.Msg;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -23,11 +28,15 @@ import javax.inject.Inject;
 public class PitView implements Serializable {
     private List<Pitpoz> lista;
     @Inject private PitDAO pitDAO;
+    @Inject private PodatnikDAO podatnikDAO;
     @ManagedProperty(value="#{WpisView}")
     private WpisView wpisView;
+    @Inject private Pitpoz biezacyPit;
+   
 
     public PitView() {
         lista = new ArrayList<>();
+       
     }
     
     
@@ -35,7 +44,18 @@ public class PitView implements Serializable {
     @PostConstruct
     private void init(){
         lista = (List<Pitpoz>) pitDAO.findPitPod(wpisView.getRokWpisu().toString(), wpisView.getPodatnikWpisu());
+       
     }
+    
+     public void usun() {
+        int index = lista.size() - 1;
+        Pitpoz selected = lista.get(index);
+        pitDAO.destroy(selected);
+        lista.remove(selected);
+        RequestContext.getCurrentInstance().update("formpi:");
+        Msg.msg("i", "Usunieto parametr ZUS do podatnika "+selected.getUdzialowiec()+" za m-c: "+selected.getPkpirM(),"formpi:messages");
+    }
+
 
     public List<Pitpoz> getLista() {
         return lista;
@@ -60,7 +80,16 @@ public class PitView implements Serializable {
     public void setWpisView(WpisView wpisView) {
         this.wpisView = wpisView;
     }
-    
+
+    public Pitpoz getBiezacyPit() {
+        return biezacyPit;
+    }
+
+    public void setBiezacyPit(Pitpoz biezacyPit) {
+        this.biezacyPit = biezacyPit;
+    }
+
+   
     
 }
 
