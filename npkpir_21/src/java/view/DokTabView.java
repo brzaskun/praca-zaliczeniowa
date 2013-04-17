40baf080bfed181233d341da7648652d9d080b3f
Brzaskun <Brzaskun@gmail.com>
2013-04-17 19:46:00 +0000

    sumowanie vat-ue kod kraju poprawiono sumowanie i wyswietlanie

--- npkpir_21/src/java/view/DokTabView.java
+++ npkpir_21/src/java/view/DokTabView.java
@@ -34,7 +34,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
--- npkpir_21/src/java/view/VatUeView.java
+++ npkpir_21/src/java/view/VatUeView.java
@@ -6,7 +6,9 @@ package view;
 
 import comparator.Dokcomparator;
 import dao.DokDAO;
+import embeddable.Kwartaly;
 import embeddable.Mce;
+import embeddable.VatUe;
 import entity.Dok;
 import entity.Klienci;
 import entity.Podatnik;
@@ -49,7 +51,7 @@ public class VatUeView implements Serializable {
     private List<Dok> listawybranych;
     @Inject private Uz uzytkownik;
     //lista gdzie beda podsumowane wartosci
-    private List<Dok> podsumowanie;
+    private List<VatUe> podsumowanie;
     
     public VatUeView() {
         //dokumenty podatnika
@@ -63,14 +65,14 @@ public class VatUeView implements Serializable {
     }
 
     @PostConstruct
-    public void init() {
+    public void init() throws Exception {
             Integer rok = wpisView.getRokWpisu();
             String mc = wpisView.getMiesiacWpisu();
             String podatnik = wpisView.getPodatnikWpisu();
             Podatnik pod = wpisView.getPodatnikObiekt();
             uzytkownik = wpisView.getWprowadzil();
             try {
-                obiektDOKjsfSel.addAll(dokDAO.zwrocBiezacegoKlienta(wpisView.getPodatnikWpisu()));
+                obiektDOKjsfSel.addAll(dokDAO.zwrocBiezacegoKlientaRok(wpisView.getPodatnikWpisu(),wpisView.getRokWpisu().toString()));
                 //sortowanie dokumentów
                     Collections.sort(obiektDOKjsfSel, new Dokcomparator());
                 //
@@ -85,48 +87,49 @@ public class VatUeView implements Serializable {
             Integer m1 = Integer.parseInt(m);
             String mn = Mce.getMapamcy().get(m1);
             Integer r = wpisView.getRokWpisu();
-            Set<Klienci> klienty = new HashSet<>();
-            int numerkolejny = 1;
-            for(Dok tmpx : obiektDOKjsfSel){
-                if (tmpx.getPkpirR().equals(r.toString())) {
-                    tmpx.setNrWpkpir(numerkolejny++);
-                    obiektDOKjsfSelRok.add(tmpx);
-                    if (tmpx.getVatM().equals(mn)) {
-                        if(tmpx.getTypdokumentu().equals("WNT")||tmpx.getTypdokumentu().equals("WDT")){
-                        dokvatmc.add(tmpx);
-                        klienty.add(tmpx.getKontr());
-                        }
-                    }
-                }
-            }
+            dokvatmc.addAll(zmodyfikujliste(obiektDOKjsfSel, "kwartał"));
             //a teraz podsumuj klientów
-            ListIterator itr;
-            itr = dokvatmc.listIterator();
-            while(itr.hasNext()){
-                Dok p = (Dok) itr.next();
-                if(podsumowanie.isEmpty()){
-                    podsumowanie.add(p);
-                    itr.remove();
-                } else {
-                ListIterator it;
-                it = podsumowanie.listIterator();
-                 while(it.hasNext()){
-                        Dok s = (Dok) it.next();
-                        double tmp = 0.0;
-                       if(p.getKontr().getNip().equals(s.getKontr().getNip())){
-                           tmp = s.getNetto();
-                           tmp += p.getNetto();
-                           s.setNetto(tmp);
-                           break;
-                       } else {
-                           it.add(p);
+            for(Dok p : dokvatmc){
+                for(VatUe s : podsumowanie){
+                       if(p.getKontr().getNip().equals(s.getKontrahent().getNip())&&p.getTypdokumentu().equals(s.getTransakcja())){
+                           s.setNetto(p.getNetto()+s.getNetto());
+                           s.setLiczbadok(s.getLiczbadok()+1);
+                           s.getZawiera().add(p);
                            break;
                        }
                     }
                 }
+            System.out.println("l");
             }
-    }
-            
+    
+      private List<Dok> zmodyfikujliste(List<Dok> listadokvat, String vatokres) throws Exception {
+        Set<VatUe> klienty = new HashSet<>();
+         if(vatokres.equals("blad")){
+            throw new Exception("Nie ma ustawionego parametru vat za dany okres");
+         } else if (vatokres.equals("miesięczne")){
+             List<Dok> listatymczasowa = new ArrayList<>();
+             for(Dok p : listadokvat){
+                 if(p.getVatM().equals(wpisView.getMiesiacWpisu())){
+                     listatymczasowa.add(p);
+                 }
+             }
+             return listatymczasowa;
+         } else {
+             List<Dok> listatymczasowa = new ArrayList<>();
+             Integer kwartal = Integer.parseInt(Kwartaly.getMapanrkw().get(Integer.parseInt(wpisView.getMiesiacWpisu())));
+             List<String> miesiacewkwartale = Kwartaly.getMapakwnr().get(kwartal);
+             for(Dok p : listadokvat){
+                 if(p.getVatM().equals(miesiacewkwartale.get(0))||p.getVatM().equals(miesiacewkwartale.get(1))||p.getVatM().equals(miesiacewkwartale.get(2))){
+                     listatymczasowa.add(p);
+                 }
+                 if(p.getTypdokumentu().equals("WNT")||p.getTypdokumentu().equals("WDT")){
+                    klienty.add(new VatUe(p.getTypdokumentu(),p.getKontr(),0.0,0,new ArrayList<Dok>()));
+             }
+             }
+             podsumowanie.addAll(klienty);
+             return listatymczasowa;
+         }
+    }       
 
     public List<Dok> getDokvatmc() {
         return dokvatmc;
@@ -152,14 +155,15 @@ public class VatUeView implements Serializable {
         this.listawybranych = listawybranych;
     }
 
-    public List<Dok> getPodsumowanie() {
+    public List<VatUe> getPodsumowanie() {
         return podsumowanie;
     }
 
-    public void setPodsumowanie(List<Dok> podsumowanie) {
+    public void setPodsumowanie(List<VatUe> podsumowanie) {
         this.podsumowanie = podsumowanie;
     }
 
+   
    
  
 }
