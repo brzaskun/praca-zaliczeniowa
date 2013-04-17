38ea464a28542d6c601f46568e7ee5a6a8f34264
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-04-17 19:46:00 +0000

    sumowanie vat-ue kod kraju poprawiono sumowanie i wyswietlanie

--- trunk/npkpir_21/src/java/view/ObrotyView.java
+++ trunk/npkpir_21/src/java/view/ObrotyView.java
@@ -46,6 +46,7 @@ public class ObrotyView implements Serializable{
      //lista wybranych dokumentow w panelu Guest
     private List<Dok> goscwybral;
     private Double podsumowaniewybranych;
+    private Double podsumowaniewybranychnetto;
 
     public ObrotyView() {
          //dokumenty podatnika za okres od-do
@@ -124,10 +125,14 @@ public class ObrotyView implements Serializable{
            
    public void sumawartosciwybranych(){
         podsumowaniewybranych = 0.0;
+        podsumowaniewybranychnetto = 0.0;
         for(Dok p : goscwybral){
             podsumowaniewybranych += p.getBrutto();
+             podsumowaniewybranychnetto += p.getNetto();
         }
     }
+   
+       
     public List<Dok> getObiektDOKmrjsfSelX() {
         return obiektDOKmrjsfSelX;
     }
@@ -161,5 +166,13 @@ public class ObrotyView implements Serializable{
         this.wpisView = wpisView;
     }
 
-     
+    public Double getPodsumowaniewybranychnetto() {
+        return podsumowaniewybranychnetto;
+    }
+
+    public void setPodsumowaniewybranychnetto(Double podsumowaniewybranychnetto) {
+        this.podsumowaniewybranychnetto = podsumowaniewybranychnetto;
+    }
+
+    
 }
