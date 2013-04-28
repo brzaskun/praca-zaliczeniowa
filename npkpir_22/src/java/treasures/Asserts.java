becc1b0998b4c1d74570adc9fd560fb9183ede23
Brzaskun <Brzaskun@gmail.com>
2013-04-28 21:51:04 +0000

    npkpir22

---
+++ npkpir_22/src/java/treasures/Asserts.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treasures;
+
+/**
+ *
+ * @author Osito
+ */
+public class Asserts {
+    public static void main(String[] args){
+         Object obj = "kotek";
+         assert obj instanceof Double  : "Tu powinien byc String";
+    }
+}
