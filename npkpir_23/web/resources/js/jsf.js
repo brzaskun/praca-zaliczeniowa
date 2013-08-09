854980cc4a68606c93a06b960f3fd6feb34f7934
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-08-09 18:15:47 +0000

    pare drobnych poprawek ulatwiajaco upiekszajacych

--- npkpir_23/web/resources/js/jsf.js
+++ npkpir_23/web/resources/js/jsf.js
@@ -143,6 +143,12 @@ var aktywujopis = function (){
         $('#dodWiad\\:inwestycja').hide();
         $('#dodWiad\\:inwestycjas').hide();
     }
+    if(dokument==='LP'){
+        $('#dodWiad\\:dokumentprosty').attr('checked', true);
+    }
+    if(dokument==='ZUS'){
+        $('#dodWiad\\:dokumentprosty').attr('checked', true);
+    }
     $('#dodWiad\\:opis').on('keydown',function(e){
         if(e.which=='120'){
             $('#dodWiad\\:dodajopis').click();
