a69c5d9f352cd8410691daa06e8fc4936d3551b6
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-09-08 16:54:52 +0000

    poprawiam czesc goscia

--- npkpir_23/src/java/entityfk/Dokfk.java
+++ npkpir_23/src/java/entityfk/Dokfk.java
@@ -29,130 +29,130 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author Osito
  */
 //@Entity
-//@Table(catalog = "pkpir", schema = "")
-//@XmlRootElement
-//@NamedQueries({
-//    @NamedQuery(name = "Dokfk.findAll", query = "SELECT d FROM Dokfk d"),
-//    @NamedQuery(name = "Dokfk.findBySeriadokfk", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.seriadokfk = :seriadokfk"),
-//    @NamedQuery(name = "Dokfk.findByNrkolejny", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.nrkolejny = :nrkolejny"),
-//    @NamedQuery(name = "Dokfk.findByPodatnik", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.podatnik = :podatnik"),
-//    @NamedQuery(name = "Dokfk.findByDatawystawienia", query = "SELECT d FROM Dokfk d WHERE d.datawystawienia = :datawystawienia"),
-//    @NamedQuery(name = "Dokfk.findByNaniesionezapisy", query = "SELECT d FROM Dokfk d WHERE d.naniesionezapisy = :naniesionezapisy"),
-//    @NamedQuery(name = "Dokfk.findByNumer", query = "SELECT d FROM Dokfk d WHERE d.numer = :numer")})
+@Table(catalog = "pkpir", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Dokfk.findAll", query = "SELECT d FROM Dokfk d"),
+    @NamedQuery(name = "Dokfk.findBySeriadokfk", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.seriadokfk = :seriadokfk"),
+    @NamedQuery(name = "Dokfk.findByNrkolejny", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.nrkolejny = :nrkolejny"),
+    @NamedQuery(name = "Dokfk.findByPodatnik", query = "SELECT d FROM Dokfk d WHERE d.dokfkPK.podatnik = :podatnik"),
+    @NamedQuery(name = "Dokfk.findByDatawystawienia", query = "SELECT d FROM Dokfk d WHERE d.datawystawienia = :datawystawienia"),
+    @NamedQuery(name = "Dokfk.findByNaniesionezapisy", query = "SELECT d FROM Dokfk d WHERE d.naniesionezapisy = :naniesionezapisy"),
+    @NamedQuery(name = "Dokfk.findByNumer", query = "SELECT d FROM Dokfk d WHERE d.numer = :numer")})
 public class Dokfk implements Serializable {
-//    private static final long serialVersionUID = 1L;
-//    @EmbeddedId
-//     @AttributeOverrides({
-//        @AttributeOverride(name = "seriadokfk", column = @Column(name = "seriadokfk")),
-//        @AttributeOverride(name = "nrkolejny", column = @Column(name = "nrkolejny")),
-//        @AttributeOverride(name = "podatnik", column = @Column(name = "podatnik"))
-//    })
-//    protected DokfkPK dokfkPK;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Size(min = 1, max = 10)
-//    @Column(nullable = false, length = 10)
-//    private String datawystawienia;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Column(nullable = false)
-//    private boolean naniesionezapisy;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Size(min = 1, max = 255)
-//    @Column(nullable = false, length = 255)
-//    private String numer;
-//    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinColumn(name = "seriadokfk")
-//    private List<Wiersze> konta;
-//
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+     @AttributeOverrides({
+        @AttributeOverride(name = "seriadokfk", column = @Column(name = "seriadokfk")),
+        @AttributeOverride(name = "nrkolejny", column = @Column(name = "nrkolejny")),
+        @AttributeOverride(name = "podatnik", column = @Column(name = "podatnik"))
+    })
+    protected DokfkPK dokfkPK;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(nullable = false, length = 10)
+    private String datawystawienia;
+    @Basic(optional = false)
+    @NotNull
+    @Column(nullable = false)
+    private boolean naniesionezapisy;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(nullable = false, length = 255)
+    private String numer;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "seriadokfk")
+    private List<Wiersze> konta;
+
+    
+    
+    
+    public Dokfk() {
+    }
+
+    public Dokfk(DokfkPK dokfkPK) {
+        this.dokfkPK = dokfkPK;
+    }
+
+    public Dokfk(DokfkPK dokfkPK, String datawystawienia, boolean naniesionezapisy, String numer) {
+        this.dokfkPK = dokfkPK;
+        this.datawystawienia = datawystawienia;
+        this.naniesionezapisy = naniesionezapisy;
+        this.numer = numer;
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+    public Dokfk(String seriadokfk, int nrkolejny, String podatnik) {
+        this.dokfkPK = new DokfkPK(seriadokfk, nrkolejny, podatnik);
+    }
+    
+    public DokfkPK getDokfkPK() {
+        return dokfkPK;
+    }
+    
+    public void setDokfkPK(DokfkPK dokfkPK) {
+        this.dokfkPK = dokfkPK;
+    }
+    
+    public String getDatawystawienia() {
+        return datawystawienia;
+    }
+    
+    public void setDatawystawienia(String datawystawienia) {
+        this.datawystawienia = datawystawienia;
+    }
+    
+    public boolean getNaniesionezapisy() {
+        return naniesionezapisy;
+    }
+    
+    public void setNaniesionezapisy(boolean naniesionezapisy) {
+        this.naniesionezapisy = naniesionezapisy;
+    }
+    
+    public String getNumer() {
+        return numer;
+    }
+    
+    public void setNumer(String numer) {
+        this.numer = numer;
+    }
 //    
-//    
-//    
-//    public Dokfk() {
-//    }
-//
-//    public Dokfk(DokfkPK dokfkPK) {
-//        this.dokfkPK = dokfkPK;
-//    }
-//
-//    public Dokfk(DokfkPK dokfkPK, String datawystawienia, boolean naniesionezapisy, String numer) {
-//        this.dokfkPK = dokfkPK;
-//        this.datawystawienia = datawystawienia;
-//        this.naniesionezapisy = naniesionezapisy;
-//        this.numer = numer;
-//    }
-//
-//    //<editor-fold defaultstate="collapsed" desc="comment">
-//    public Dokfk(String seriadokfk, int nrkolejny, String podatnik) {
-//        this.dokfkPK = new DokfkPK(seriadokfk, nrkolejny, podatnik);
-//    }
-//    
-//    public DokfkPK getDokfkPK() {
-//        return dokfkPK;
-//    }
-//    
-//    public void setDokfkPK(DokfkPK dokfkPK) {
-//        this.dokfkPK = dokfkPK;
-//    }
-//    
-//    public String getDatawystawienia() {
-//        return datawystawienia;
-//    }
-//    
-//    public void setDatawystawienia(String datawystawienia) {
-//        this.datawystawienia = datawystawienia;
-//    }
-//    
-//    public boolean getNaniesionezapisy() {
-//        return naniesionezapisy;
-//    }
-//    
-//    public void setNaniesionezapisy(boolean naniesionezapisy) {
-//        this.naniesionezapisy = naniesionezapisy;
-//    }
-//    
-//    public String getNumer() {
-//        return numer;
-//    }
-//    
-//    public void setNumer(String numer) {
-//        this.numer = numer;
-//    }
-////    
-////    @XmlTransient
-////    public List<Wiersze> getKonta() {
-////        return konta;
-////    }
-////    
-////    public void setKonta(List<Wiersze> konta) {
-////        this.konta = konta;
-////    }
-//    //</editor-fold>
-//
-//    @Override
-//    public int hashCode() {
-//        int hash = 0;
-//        hash += (dokfkPK != null ? dokfkPK.hashCode() : 0);
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Object object) {
-//        // TODO: Warning - this method won't work in the case the id fields are not set
-//        if (!(object instanceof Dokfk)) {
-//            return false;
-//        }
-//        Dokfk other = (Dokfk) object;
-//        if ((this.dokfkPK == null && other.dokfkPK != null) || (this.dokfkPK != null && !this.dokfkPK.equals(other.dokfkPK))) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "entityfk.Dokfk[ dokfkPK=" + dokfkPK + " ]";
+//    @XmlTransient
+//    public List<Wiersze> getKonta() {
+//        return konta;
 //    }
 //    
+//    public void setKonta(List<Wiersze> konta) {
+//        this.konta = konta;
+//    }
+    //</editor-fold>
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (dokfkPK != null ? dokfkPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Dokfk)) {
+            return false;
+        }
+        Dokfk other = (Dokfk) object;
+        if ((this.dokfkPK == null && other.dokfkPK != null) || (this.dokfkPK != null && !this.dokfkPK.equals(other.dokfkPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entityfk.Dokfk[ dokfkPK=" + dokfkPK + " ]";
+    }
+    
 }
--- npkpir_23/src/java/entityfk/DokfkPK.java
+++ npkpir_23/src/java/entityfk/DokfkPK.java
@@ -17,85 +17,85 @@ import javax.validation.constraints.Size;
  */
 //@Embeddable
 public class DokfkPK implements Serializable {
-//    @Basic(optional = false)
-//    @NotNull
-//    @Size(min = 1, max = 5)
-//    @Column(nullable = false, length = 5)
-//    private String seriadokfk;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Column(nullable = false)
-//    private int nrkolejny;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Size(min = 1, max = 255)
-//    @Column(nullable = false, length = 255)
-//    private String podatnik;
-//
-//    public DokfkPK() {
-//    }
-//
-//    public DokfkPK(String seriadokfk, int nrkolejny, String podatnik) {
-//        this.seriadokfk = seriadokfk;
-//        this.nrkolejny = nrkolejny;
-//        this.podatnik = podatnik;
-//    }
-//
-//    public String getSeriadokfk() {
-//        return seriadokfk;
-//    }
-//
-//    public void setSeriadokfk(String seriadokfk) {
-//        this.seriadokfk = seriadokfk;
-//    }
-//
-//    public int getNrkolejny() {
-//        return nrkolejny;
-//    }
-//
-//    public void setNrkolejny(int nrkolejny) {
-//        this.nrkolejny = nrkolejny;
-//    }
-//
-//    public String getPodatnik() {
-//        return podatnik;
-//    }
-//
-//    public void setPodatnik(String podatnik) {
-//        this.podatnik = podatnik;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int hash = 0;
-//        hash += (seriadokfk != null ? seriadokfk.hashCode() : 0);
-//        hash += (int) nrkolejny;
-//        hash += (podatnik != null ? podatnik.hashCode() : 0);
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Object object) {
-//        // TODO: Warning - this method won't work in the case the id fields are not set
-//        if (!(object instanceof DokfkPK)) {
-//            return false;
-//        }
-//        DokfkPK other = (DokfkPK) object;
-//        if ((this.seriadokfk == null && other.seriadokfk != null) || (this.seriadokfk != null && !this.seriadokfk.equals(other.seriadokfk))) {
-//            return false;
-//        }
-//        if (this.nrkolejny != other.nrkolejny) {
-//            return false;
-//        }
-//        if ((this.podatnik == null && other.podatnik != null) || (this.podatnik != null && !this.podatnik.equals(other.podatnik))) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "entityfk.DokfkPK[ seriadokfk=" + seriadokfk + ", nrkolejny=" + nrkolejny + ", podatnik=" + podatnik + " ]";
-//    }
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 5)
+    @Column(nullable = false, length = 5)
+    private String seriadokfk;
+    @Basic(optional = false)
+    @NotNull
+    @Column(nullable = false)
+    private int nrkolejny;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(nullable = false, length = 255)
+    private String podatnik;
+
+    public DokfkPK() {
+    }
+
+    public DokfkPK(String seriadokfk, int nrkolejny, String podatnik) {
+        this.seriadokfk = seriadokfk;
+        this.nrkolejny = nrkolejny;
+        this.podatnik = podatnik;
+    }
+
+    public String getSeriadokfk() {
+        return seriadokfk;
+    }
+
+    public void setSeriadokfk(String seriadokfk) {
+        this.seriadokfk = seriadokfk;
+    }
+
+    public int getNrkolejny() {
+        return nrkolejny;
+    }
+
+    public void setNrkolejny(int nrkolejny) {
+        this.nrkolejny = nrkolejny;
+    }
+
+    public String getPodatnik() {
+        return podatnik;
+    }
+
+    public void setPodatnik(String podatnik) {
+        this.podatnik = podatnik;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (seriadokfk != null ? seriadokfk.hashCode() : 0);
+        hash += (int) nrkolejny;
+        hash += (podatnik != null ? podatnik.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DokfkPK)) {
+            return false;
+        }
+        DokfkPK other = (DokfkPK) object;
+        if ((this.seriadokfk == null && other.seriadokfk != null) || (this.seriadokfk != null && !this.seriadokfk.equals(other.seriadokfk))) {
+            return false;
+        }
+        if (this.nrkolejny != other.nrkolejny) {
+            return false;
+        }
+        if ((this.podatnik == null && other.podatnik != null) || (this.podatnik != null && !this.podatnik.equals(other.podatnik))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entityfk.DokfkPK[ seriadokfk=" + seriadokfk + ", nrkolejny=" + nrkolejny + ", podatnik=" + podatnik + " ]";
+    }
     
 }
