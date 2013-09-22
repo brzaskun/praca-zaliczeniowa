acaeeedd9e92e77e94d38fbb9364ae34fb5b0230
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-09-22 09:31:53 +0000

    dziala to co chcialem, jest bidirectional onetomany relationship z zachowaniem. teraz popracuje nad kluczem zlozonym

---
+++ trunk/Bazadanych/src/java/view/DokfkView.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import entity.Dokfk;
+import entity.Wiersz;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+import msg.Msg;
+import session.DokfkFacade;
+import session.WierszFacade;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@RequestScoped
+public class DokfkView implements Serializable{
+    private Dokfk dokfk;
+    private Wiersz wiersz;
+    private static ArrayList<Dokfk> dokfklist;
+    @Inject private DokfkFacade dokfkFacade;
+    @Inject private WierszFacade wierszFacade;
+
+    public DokfkView() {
+        dokfklist = new ArrayList<>();
+        dokfk = new Dokfk();
+        wiersz = new Wiersz();
+    }
+
+    @PostConstruct
+    private void init() {
+        try {
+            dokfklist.addAll(dokfkFacade.findAll());
+        } catch (Exception e) {}
+    }
+    
+    
+    public void dodaj() {
+        List<Wiersz> listatwierszy = new ArrayList<>();
+        wiersz.setIddokumentuobcy(dokfk);
+        listatwierszy.add(wiersz);
+        dokfk.setWierszList(listatwierszy);
+        dokfkFacade.edit(dokfk);
+        Msg.msg("i", "Dodano dokument "+dokfk.getOpisdokumentu());
+        Msg.msg("i", "Dodano wiersz "+wiersz.getOpiswiersza());
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+    public Dokfk getDokfk() {
+        return dokfk;
+    }
+    
+    public void setDokfk(Dokfk dokfk) {
+        this.dokfk = dokfk;
+    }
+    
+    public ArrayList<Dokfk> getDokfklist() {
+        return dokfklist;
+    }
+    
+    public static ArrayList<Dokfk> getDokfklistS() {
+        return dokfklist;
+    }
+    
+    public void setDokfklist(ArrayList<Dokfk> dokfklist) {
+        this.dokfklist = dokfklist;
+    }
+        
+    public Wiersz getWiersz() {
+        return wiersz;
+    }
+
+    public void setWiersz(Wiersz wiersz) {
+        this.wiersz = wiersz;
+    }
+    
+    
+    //</editor-fold>
+
+    
+}
