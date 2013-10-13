67cdc3ba87795f1e0fa889bcd2f2865b8787a3a8
Brzaskun <Brzaskun@gmail.com>
2013-10-13 16:19:27 +0000

    dodawanie dokumnetu z zawartymi wierszami co sa zachowane w osobnej tabeli. usuniecie dokumentu podowuje usuniecie wierszy z drugiej tabeli. usuniecie jednego tylko wiersza z dokumentu powoduje usuniecie go z wlasciwej tabeli

--- Bazadanych/src/java/view/DokfkView.java
+++ Bazadanych/src/java/view/DokfkView.java
@@ -9,10 +9,10 @@ import entity.DokfkPK;
 import entity.Wiersz;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import javax.inject.Inject;
 import msg.Msg;
 import session.DokfkFacade;
@@ -23,42 +23,74 @@ import session.WierszFacade;
  * @author Osito
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class DokfkView implements Serializable{
     private Dokfk dokfk;
-    private DokfkPK dokfkPK;
     private Wiersz wiersz;
     private static ArrayList<Dokfk> dokfklist;
+    private static ArrayList<Wiersz> wierszelista;
     @Inject DokfkFacade dokfkFacade;
     @Inject private WierszFacade wierszFacade;
 
     public DokfkView() {
         dokfklist = new ArrayList<>();
         dokfk = new Dokfk();
-        dokfkPK = new DokfkPK();
+        dokfk.setDokfkPK(new DokfkPK());
         wiersz = new Wiersz();
+        wierszelista = new ArrayList<>();
     }
 
     @PostConstruct
     private void init() {
         try {
             dokfklist.addAll(dokfkFacade.findAll());
+            wierszelista.add(new Wiersz());
         } catch (Exception e) {}
     }
     
     
     public void dodaj() {
-        List<Wiersz> listatwierszy = new ArrayList<>();
-        //wiersz.setIddokumentuobcy(dokfk);
-        wiersz.setDokfk(dokfk);
-        listatwierszy.add(wiersz);
-        dokfk.setWierszList(listatwierszy);
-        dokfk.setDokfkPK(dokfkPK);
+        for (Wiersz p : wierszelista) {
+            p.setDokfk(dokfk);
+        }
+        dokfk.setWierszList(wierszelista);
         dokfkFacade.edit(dokfk);
         Msg.msg("i", "Dodano dokument "+dokfk.getOpisdokumentu());
-        Msg.msg("i", "Dodano wiersz "+wiersz.getOpiswiersza());
+        Msg.msg("i", "Dodano wiersz "+wierszelista.toString());
     }
+    
+     public void edit() {
+        dokfkFacade.edit(dokfk);
+        Msg.msg("i", "Wyedtowano dokument "+dokfk.getOpisdokumentu());
+    }
+     
+     
 
+     public void edytujopisdokumenty(ValueChangeEvent event) {
+            Object source = event.getSource();
+            Object oldValue = event.getOldValue();
+            Msg.msg("i", "Stara wartosc obiektu: "+source+" to "+oldValue);
+            Object newValue = event.getNewValue();
+            Msg.msg("i", "Nowa wartosc "+source+" to "+newValue);
+            // ...
+        }
+     
+     public void usun(Dokfk dokfkusun) {
+         dokfklist.remove(dokfkusun);
+         dokfkFacade.remove(dokfkusun);
+     }
+     
+     public void usunwiersz(Integer index) {
+        Wiersz dokwiersz = dokfk.getWierszList().get(index);
+        dokfk.getWierszList().remove(dokwiersz);
+        dokfkFacade.edit(dokfk);
+        Msg.msg("i", "Wyedtowano dokument "+dokfk.getOpisdokumentu());
+    }
+     
+     public void dodajwiersz() {
+         wierszelista.add(new Wiersz());
+     }
+    
     //<editor-fold defaultstate="collapsed" desc="comment">
     public Dokfk getDokfk() {
         return dokfk;
@@ -88,16 +120,18 @@ public class DokfkView implements Serializable{
         this.wiersz = wiersz;
     }
     
-    public DokfkPK getDokfkPK() {
-        return dokfkPK;
+    public ArrayList<Wiersz> getWierszelista() {
+        return wierszelista;
     }
 
-    public void setDokfkPK(DokfkPK dokfkPK) {
-        this.dokfkPK = dokfkPK;
+    public void setWierszelista(ArrayList<Wiersz> wierszelista) {
+        DokfkView.wierszelista = wierszelista;
     }
+     
     
     //</editor-fold>
 
+      
     
 
     
