d026ed270333d35ff744d740b73d366ddc6642aa
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-10-21 17:41:48 +0000

    dodalem nowe menu, nowa strone (rozrachunki) i zlikiwdowalem menudialogi. teraz biore sie za rozrachunki

--- npkpir_23/web/resources/js_fk/dialog.js
+++ npkpir_23/web/resources/js_fk/dialog.js
@@ -1,13 +1,17 @@
-var ustaw = function(nazwa) {
+var ustawdialog = function(nazwa,menu) {
+    try {
         $(document.getElementById(nazwa)).position({
         my: "left top",
         at: "center top",
-        of: $("#dialogmenudokumenty"),
+        of: $(document.getElementById(menu)),
         collision: "none none"
     });
     $(document.getElementById(nazwa)).width(1250).height(700);
+    } catch (Exception) {
+        alert ("blad w fukncji ustaw w pliku dialog.js wiersz 1 "+Exception);
+    }
 
 };
-var resetuj = function(nazwa) {
+var resetujdialog = function(nazwa) {
     $(document.getElementById(nazwa)).removeAttr('style');
 };
