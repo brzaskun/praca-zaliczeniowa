657f4aeb6c97dafcbffc9791911be9be10f1e099
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-10-29 20:32:38 +0000

    działa skroling z lazeniem po wpisach w tabeli w uproszczonej :)

--- npkpir_23/web/resources/js_pkpir/tablewalking.js
+++ npkpir_23/web/resources/js_pkpir/tablewalking.js
@@ -9,6 +9,12 @@ var przejdzwiersz = function () {
         }
     }
   var komorki = $(lolo[MYAPP.nrbiezacegowiersza]).children("td");
+  var czynaekranie = isScrolledIntoView(komorki[1]);
+  if (!czynaekranie) {
+    var wysokosc = 70;
+    var elem = document.getElementById('form:dokumentyLista');
+    elem.scrollTop = elem.scrollTop + wysokosc;
+  }
   $(komorki[1]).click();
 };
 
@@ -23,7 +29,25 @@ var wrocwiersz = function () {
         }
     }
   var komorki = $(lolo[MYAPP.nrbiezacegowiersza]).children("td");
+  var czynaekranie = isScrolledIntoView(komorki[1]);
+  if (!czynaekranie) {
+    var wysokosc = 70;
+    var elem = document.getElementById('form:dokumentyLista');
+    elem.scrollTop = elem.scrollTop - wysokosc;
+  }
   $(komorki[1]).click();
 };
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop()+150;
+    var docViewBottom = docViewTop + $(window).height()-300;
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+      && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+}
 
 
