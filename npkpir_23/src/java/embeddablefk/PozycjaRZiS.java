4433c1b005480955a4f904f39232087bf62e57e7
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-12-08 23:26:42 +0000

    ucze sie klasy TreeNode

---
+++ npkpir_23/src/java/embeddablefk/PozycjaRZiS.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package embeddablefk;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Osito
+ */
+public class PozycjaRZiS implements Serializable{
+    private int pozycjanr;
+    private String pozycjaString;
+    private String pozycjaSymbol;
+    private int macierzysty;
+    private int level;
+    private String nazwa;
+    private boolean przychod0koszt1;
+    private int lp;
+
+    public PozycjaRZiS(PozycjaRZiS pozycjaRZiS) {
+        this.pozycjanr = pozycjaRZiS.getPozycjanr();
+        this.pozycjaString = pozycjaRZiS.getPozycjaString();
+        this.pozycjaSymbol = pozycjaRZiS.getPozycjaSymbol();
+        this.macierzysty = pozycjaRZiS.getMacierzysty();
+        this.level = pozycjaRZiS.getLevel();
+        this.nazwa = pozycjaRZiS.getNazwa();
+        this.przychod0koszt1 = pozycjaRZiS.isPrzychod0koszt1();
+        this.lp = pozycjaRZiS.getLp();
+    }
+
+    public PozycjaRZiS(int pozycjanr, String pozycjaString, String pozycjaSymbol, int macierzysty, int level, String nazwa, boolean przychod0koszt1, int lp) {
+        
+    }
+    
+
+    public PozycjaRZiS(int lp, String pozycjaString, String pozycjaSymbol, int macierzysty, int level, String nazwa, boolean przychod0koszt1) {
+        this.lp = lp;
+        this.pozycjaString = pozycjaString;
+        this.pozycjaSymbol = pozycjaSymbol;
+        this.macierzysty = macierzysty;
+        this.level = level;
+        this.nazwa = nazwa;
+        this.przychod0koszt1 = przychod0koszt1;
+    }
+
+    
+    
+    public int getPozycjanr() {
+        return pozycjanr;
+    }
+
+    public void setPozycjanr(int pozycjanr) {
+        this.pozycjanr = pozycjanr;
+    }
+
+    public String getPozycjaString() {
+        return pozycjaString;
+    }
+
+    public void setPozycjaString(String pozycjaString) {
+        this.pozycjaString = pozycjaString;
+    }
+
+    public String getPozycjaSymbol() {
+        return pozycjaSymbol;
+    }
+
+    public void setPozycjaSymbol(String pozycjaSymbol) {
+        this.pozycjaSymbol = pozycjaSymbol;
+    }
+
+    public int getMacierzysty() {
+        return macierzysty;
+    }
+
+    public void setMacierzysty(int macierzysty) {
+        this.macierzysty = macierzysty;
+    }
+   
+     public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public boolean isPrzychod0koszt1() {
+        return przychod0koszt1;
+    }
+
+    public void setPrzychod0koszt1(boolean przychod0koszt1) {
+        this.przychod0koszt1 = przychod0koszt1;
+    }
+
+    public int getLp() {
+        return lp;
+    }
+
+    public void setLp(int lp) {
+        this.lp = lp;
+    }
+
+   
+    
+    
+    
+}
---
+++ npkpir_23/src/java/viewfk/PozycjaRZiSView.java
@@ -0,0 +1,133 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viewfk;
+
+import embeddablefk.PozycjaRZiS;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.swing.tree.DefaultMutableTreeNode;
+import org.primefaces.component.tree.Tree;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@ViewScoped
+public class PozycjaRZiSView implements Serializable {
+
+    private TreeNode root;
+    private TreeNode[] selectedNodes;
+    private PozycjaRZiS selected;
+
+    public PozycjaRZiSView() {
+        this.root = new DefaultTreeNode("root", null);
+    }
+
+    @PostConstruct
+    private void init() {
+        ArrayList<PozycjaRZiS> pozycje = new ArrayList<>();
+        pozycje.add(new PozycjaRZiS(1, "A", "A", 0, 0, "Przychody netto ze sprzedaży i zrównane z nimi, w tym:", true));
+        pozycje.add(new PozycjaRZiS(2, "A.I", "I", 1, 1, "Przychody netto ze sprzedaży produktów", true));
+        pozycje.add(new PozycjaRZiS(3, "A.I", "II", 1, 1, "Zmiana stanu produktów", true));
+        pozycje.add(new PozycjaRZiS(4, "A.I", "III", 1, 1, "Koszt wytworzenia produktów na własne potrzeby jednostki", true));
+        pozycje.add(new PozycjaRZiS(5, "A.I", "IV", 1, 1, "Przychody netto ze sprzeda􀄪y towarów i materiałów", true));
+        pozycje.add(new PozycjaRZiS(6, "B", "B", 0, 0, "Koszty działalności operacyjnej", true));
+        pozycje.add(new PozycjaRZiS(7, "B.I", "I", 6, 1, "Amortyzacja", true));
+        pozycje.add(new PozycjaRZiS(8, "B.II", "II", 6, 1, "Zużycie materiałów i energii", true));
+        pozycje.add(new PozycjaRZiS(9, "B.III", "III", 6, 1, "Usługi obce", true));
+        pozycje.add(new PozycjaRZiS(10, "B.IV", "IV", 6, 1, "Podatki i  opłaty", true));
+        pozycje.add(new PozycjaRZiS(11, "B.V", "V", 6, 1, "Wynagrodzenia", true));
+        createTreeNodesForElement(root, getElementTreeFromPlainList(pozycje));
+        DefaultTreeNode node = new DefaultTreeNode();
+        rozwin(root);
+    }
+
+    void createTreeNodesForElement(final TreeNode dmtn, final Map<String, ArrayList<PozycjaRZiS>> rzedy) {
+       ArrayList<TreeNode> poprzednie = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            ArrayList<PozycjaRZiS> biezaca = rzedy.get(String.valueOf(i));
+            for (PozycjaRZiS p : biezaca) {
+                if (i == 0) {
+                    TreeNode tmp = new DefaultTreeNode(p, root);
+                    poprzednie.add(tmp);
+                } else {
+                    Iterator it = poprzednie.iterator();
+                    while (it.hasNext()) {
+                        TreeNode r = (TreeNode) it.next();
+                        PozycjaRZiS parent = (PozycjaRZiS) r.getData();
+                        if (parent.getLp() == p.getMacierzysty()) {
+                            TreeNode tmp = new DefaultTreeNode(p, r);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //przeksztalca tresc tabeli w elementy do drzewa
+    Map<String, ArrayList<PozycjaRZiS>> getElementTreeFromPlainList(ArrayList<PozycjaRZiS> pozycje) {
+        Map<String, ArrayList<PozycjaRZiS>> rzedy = new LinkedHashMap<>(4);
+        // builds a map of elements object returned from store
+        for (int i = 0; i < 3; i++) {
+            ArrayList<PozycjaRZiS> values = new ArrayList<>();
+            for (PozycjaRZiS s : pozycje) {
+                if (s.getLevel() == i) {
+                    values.add(s);
+                }
+            }
+            rzedy.put(String.valueOf(i), values);
+        }
+        return rzedy;
+    }
+
+    private void rozwin(TreeNode n) {
+        boolean madzieci = n.getChildren().size() > 0;
+        if (madzieci == true) {
+            for (TreeNode o : n.getChildren()) {
+                o.setExpanded(true);
+                rozwin(o);
+            }
+        }
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public PozycjaRZiS getSelected() {
+        return selected;
+    }
+
+    public void setSelected(PozycjaRZiS selected) {
+        this.selected = selected;
+    }
+
+    public TreeNode[] getSelectedNodes() {
+        return selectedNodes;
+    }
+
+    public void setSelectedNodes(TreeNode[] selectedNodes) {
+        this.selectedNodes = selectedNodes;
+    }
+    //</editor-fold>
+}
