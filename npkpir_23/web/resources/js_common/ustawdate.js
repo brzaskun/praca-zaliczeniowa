04f582e74499008da832ee10a1f3a131ff5d294a
brzaskun <brzaskun@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2013-12-28 08:40:03 +0000

    DALEJ ROBIE DROBNE POPRAWKI ZEBY PRZYSTOSOWAC SIE DO ROKU 2014

--- npkpir_23/web/resources/js_common/ustawdate.js
+++ npkpir_23/web/resources/js_common/ustawdate.js
@@ -27,6 +27,29 @@ function ustawDate(rok,mc){
          $(rozliczony).attr('checked', true);
      }
    };
+   
+function ustawDateSprzedazy(rok,mc){
+    var dataWyst = document.getElementById("dodWiad:dataSPole");
+    var wart = dataWyst.value;
+    if(mc!==10&&mc!==11&&mc!==12){
+        mc = "0"+mc;
+    }
+    var re1 = /[0-3][0-9]/;
+    var re2 = /[0-1][0-9]\S[0-3][0-9]/;
+    var re3 = /[2][0][0-9][0-9]\S[0-1][0-9]\S[0-3][0-9]/;
+     if (wart.match(re3)) {
+            dataWyst.value = wart ;
+        } else if (wart.match(re2)){
+            dataWyst.value = rok + "-"+wart;
+        } else if (wart.match(re1)){
+            dataWyst.value = rok + "-" + mc + "-" + wart ;
+        }
+     var re = /^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/;
+     var testw = dataWyst.value;
+     if (!testw.match(re)){
+         dataWyst.value = "b\u0142ędna data";
+     }
+   };
 
 function ustawDateFK(rok,mc){
     var dataWyst = document.getElementById("formwpisdokument:datka");
