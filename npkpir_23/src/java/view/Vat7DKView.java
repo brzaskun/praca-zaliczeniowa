be1743d5a3e4c14c6547f48a2951ea8bbd2085f9
Brzaskun <Brzaskun@gmail.com>
2014-03-18 21:43:37 +0000

    refaktoryzacja wysylania dekalracji vat wraz z podzialem czesci testowej i normalnej oraz obsluga bledow

--- npkpir_23/src/java/view/Vat7DKView.java
+++ npkpir_23/src/java/view/Vat7DKView.java
@@ -304,24 +304,8 @@ public class Vat7DKView implements Serializable {
      private String sprawdzjakiokresvat() {
         Integer rok = wpisView.getRokWpisu();
         Integer mc = Integer.parseInt(wpisView.getMiesiacWpisu());
-        Integer sumaszukana = rok+mc;
         List<Parametr> parametry = wpisView.getPodatnikObiekt().getVatokres();
-        //odszukaj date w parametrze - kandydat na metode statyczna
-        for(Parametr p : parametry){
-            if(p.getRokDo()!=null&&!"".equals(p.getRokDo())){
-            int wynikPo = Data.compare(rok, mc, Integer.parseInt(p.getRokOd()), Integer.parseInt(p.getMcOd()));
-            int wynikPrzed = Data.compare(rok, mc, Integer.parseInt(p.getRokDo()), Integer.parseInt(p.getMcDo()));
-            if(wynikPo > 1 && wynikPrzed < 0){
-                return p.getParametr();
-            }
-            } else {
-            int wynik = Data.compare(rok, mc, Integer.parseInt(p.getRokOd()), Integer.parseInt(p.getMcOd()));
-            if(wynik >= 0){
-                return p.getParametr();
-            }
-            }
-        }
-        return "blad";
+        return ParametrView.zwrocParametr(parametry, rok, mc);
     }
     
     private void najpierwszadeklaracja() {
@@ -337,7 +321,7 @@ public class Vat7DKView implements Serializable {
             //przechwytuje blad i jezeli sa dekalracje pozniejsze wyslane i bezbledne to kaze zajrzec do nich
             List<Deklaracjevat> pobranalistadeklaracji = new ArrayList<>();
             pobranalistadeklaracji = deklaracjevatDAO.findDeklaracjeWyslane(podatnik, rok);
-            if(pobranalistadeklaracji.size()==0){
+            if(pobranalistadeklaracji.isEmpty()){
                 throw new Exception();
             }
             for(Deklaracjevat p : pobranalistadeklaracji){
