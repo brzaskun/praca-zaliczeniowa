ff2e7a7fffbd24576a91e7bac326b03e73caf724
Brzaskun <Brzaskun@gmail.com>
2014-03-20 15:51:33 +0000

    drobne usterki po ostatnim upgradce

--- npkpir_23/web/resources/js_pkpir/jsf.js
+++ npkpir_23/web/resources/js_pkpir/jsf.js
@@ -407,6 +407,12 @@ function petlawywolujaca(lw,gr,co){
 
   }};
 
+var sprawdzczybrakklienta = function () {
+    var zawartosc = $('#dodWiad\\:acForce_input').val();
+    if(zawartosc==="nowy klient"){
+        dlg123.show();
+    }
+};
 
 var sprawdzczykopiowacklienta = function () {
     var zawartosc = $('#dodWiad\\:acForce_input').val();
