65e9bcb527a33958e2ad15c8796b2b032321af04
Brzaskun <Brzaskun@gmail.com>
2014-04-03 19:48:17 +0000

    napawilem maile admina. mozna wyslac maila testowego i w koncu traktuje go jako html teraz musze poprawic inne maile zeby ladnie wygladaly na dodatek zmiana statusu zgloszenia na zrobione powoduje wyslanie maila do adresata

--- npkpir_23/src/java/mail/MailAdmin.java
+++ npkpir_23/src/java/mail/MailAdmin.java
@@ -4,6 +4,8 @@
  */
 package mail;
 
+import entity.Pismoadmin;
+import entity.Uz;
 import java.io.Serializable;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -24,8 +26,8 @@ public class MailAdmin extends MailSetUp implements Serializable {
         try {
             message.setSubject(temat);
             MimeBodyPart mbp1 = new MimeBodyPart();
-            mbp1.setText(tresc);
-            mbp1.setHeader("Content-Type", "text/html");
+            mbp1.setContent(tresc, "text/html; charset=utf-8");
+            mbp1.setHeader("Content-Type", "text/html; charset=utf-8");
             Multipart mp = new MimeMultipart();
             mp.addBodyPart(mbp1);
             message.setContent(mp);
@@ -33,6 +35,35 @@ public class MailAdmin extends MailSetUp implements Serializable {
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    public static void usterkausunieta(Pismoadmin p, Uz uz) {
+        try {
+             Message message = logintoMailS(uz.getEmail());
+             message.setSubject("Wydruk deklaracji PIT za miesiąc");
+             // create and fill the first message part
+             MimeBodyPart mbp1 = new MimeBodyPart();
+             mbp1.setHeader("Content-Type", "text/html; charset=utf-8");
+             mbp1.setContent("Witaj "+p.getNadawca()
+                     + "<p>"+"Informujemy, że twoje zgłoszenie z dnia "+p.getDatawiadomosci()
+                     + "o treści:</p>"
+                     + "<p style=\"color: green;\">"+p.getTresc()+"</p>"
+                     + "<p>zostało załatwione pozytywnie przed admina.</p>"
+                     + "<p>Z poważaniem</p>"
+                     + "<br/>"
+                     + "<p>Administrator Programu</p>", "text/html; charset=utf-8");
+             // create the Multipart and add its parts to it
+             Multipart mp = new MimeMultipart();
+             mp.addBodyPart(mbp1);
+             
+             // add the Multipart to the message
+             message.setContent(mp);
+             Transport.send(message);
+             
+         } catch (MessagingException e) {
+             throw new RuntimeException(e);
+         }
+        
     }
     
     public static void main (String[] args) throws MessagingException {
--- npkpir_23/src/java/mail/MailSetUp.java
+++ npkpir_23/src/java/mail/MailSetUp.java
@@ -85,6 +85,30 @@ public class MailSetUp implements Serializable{
         return message;
     }
     
+    protected static  Message logintoMailS(String adreskontrahenta) throws MessagingException {
+        final String username = "info@e-taxman.pl";
+        final String password = "Pufikun7005*";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "az0066.srv.az.pl");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("info@e-taxman.pl"));
+        message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(adreskontrahenta));
+        return message;
+    }
+    
     protected  Message logintoMail(String adreskontrahenta) throws MessagingException {
         final String username = "info@e-taxman.pl";
         final String password = "Pufikun7005*";
