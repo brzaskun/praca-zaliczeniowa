d1538755ff65e8a9ca8a114a2fff9697f69cb882
Brzaskun <Brzaskun@gmail.com>
2014-04-21 19:03:54 +0000

    kody urzedow sa teraz statyczne. na razie dosc. zobaczymy ja bedzie po zmianach

--- npkpir_23/src/java/beansPdf/PdfFont.java
+++ npkpir_23/src/java/beansPdf/PdfFont.java
@@ -8,7 +8,6 @@ package beansPdf;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
