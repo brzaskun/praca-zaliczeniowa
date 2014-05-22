93fb8899822d26c0e73ef1815a6ea54acdc5a576
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2014-05-22 09:38:04 +0000

    naprawa wprowadzania zus, dodanie funkcjonalnosci uzupelnianie roku

---
+++ trunk/npkpir_23/src/java/comparator/Zusstawkicomparator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package comparator;
+
+import entity.Deklaracjevat;
+import entity.Zusstawki;
+import entity.ZusstawkiPK;
+import java.util.Comparator;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Osito
+ */
+@Named
+public class Zusstawkicomparator  implements Comparator<Zusstawki> {
+    @Override
+    public int compare(Zusstawki obP, Zusstawki obW)  {
+        int rokO1 = Integer.parseInt(obP.getZusstawkiPK().getRok());
+        int rokO2 = Integer.parseInt(obW.getZusstawkiPK().getRok());
+        int mcO1 = Integer.parseInt(obP.getZusstawkiPK().getMiesiac());
+        int mcO2 = Integer.parseInt(obW.getZusstawkiPK().getMiesiac());
+        if (rokO1 < rokO2) {
+            return -1;
+        } else if (rokO1 > rokO2) {
+            return 1;
+        } else if (rokO1 == rokO2) {
+            if (mcO1 == mcO2) {
+                return 0;
+            } else if (mcO1 < mcO2) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    
+}
