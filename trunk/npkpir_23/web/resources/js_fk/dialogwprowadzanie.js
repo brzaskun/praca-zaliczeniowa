0413269b810daab89daea113e19ed3d612d0b6bd
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2014-06-04 20:26:59 +0000

    powoli zmieniam te milusie rozrachunki

--- trunk/npkpir_23/web/resources/js_fk/dialogwprowadzanie.js
+++ trunk/npkpir_23/web/resources/js_fk/dialogwprowadzanie.js
@@ -3,10 +3,10 @@ var zachowajwiersz = function (wierszid, wnlubma) {
     MYAPP.wierszrozrachukowy = wierszid;
     MYAPP.zaznaczonepole = event.target;
     //document.getElementById("zaznaczonakomorka").innerHTML = event.target ;
-//        $(document.getElementById("wpisywaniefooter:wierszid")).val(wierszid);
-//        $(document.getElementById("wpisywaniefooter:wnlubma")).val(wnlubma);
+    $(document.getElementById("wpisywaniefooter:wierszid")).val(wierszid);
+    $(document.getElementById("wpisywaniefooter:wnlubma")).val(wnlubma);
     } catch (blad) {
-        alert("Blad w dialgowprowadzanie.js zachowaj wiersz "+blad);
+        //alert("Blad w dialgowprowadzanie.js zachowaj wiersz "+blad);
     }
 };
 
