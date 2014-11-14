688b064d6e07e4168aeedc001e7160b5bf315912
Brzaskun <Brzaskun@gmail.com>
2014-11-14 09:46:14 +0000

    naprawy biurowe

--- npkpir_23/src/java/embeddable/EwidencjaAddwiad.java
+++ npkpir_23/src/java/embeddable/EwidencjaAddwiad.java
@@ -16,7 +16,7 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class EwidencjaAddwiad implements Serializable{
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -595126647192102178L;
     
     private int lp;
     private String opis;
