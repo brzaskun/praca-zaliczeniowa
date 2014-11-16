6712b2006149d2ab4466efc3c62106ab3e50a066
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2014-11-16 15:28:15 +0000

    robie obsluge cech

---
+++ trunk/npkpir_23/src/java/viewfk/CechazapisuView.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package viewfk;
+
+import daoFK.CechazapisuDAOfk;
+import entityfk.Cechazapisu;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@ViewScoped
+public class CechazapisuView implements Serializable {
+    
+    @Inject private CechazapisuDAOfk cechazapisuDAOfk;
+    private List<Cechazapisu> pobranecechy;
+    private String nazwacechy;
+    private String rodzajcechy;
+
+    public CechazapisuView() {
+        this.pobranecechy = new ArrayList<>();
+    }
+    
+    @PostConstruct
+    private void init() {
+        pobranecechy = cechazapisuDAOfk.findAll();
+    }
+    
+    public void dodajnowacecha() {
+        Msg.msg("Dodano nową cechę");
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+    public String getNazwacechy() {
+        return nazwacechy;
+    }
+    
+    public void setNazwacechy(String nazwacechy) {
+        this.nazwacechy = nazwacechy;
+    }
+    
+    public String getRodzajcechy() {
+        return rodzajcechy;
+    }
+    
+    public void setRodzajcechy(String rodzajcechy) {
+        this.rodzajcechy = rodzajcechy;
+    }
+    
+    public List<Cechazapisu> getPobranecechy() {
+        return pobranecechy;
+    }
+    
+    public void setPobranecechy(List<Cechazapisu> pobranecechy) {
+        this.pobranecechy = pobranecechy;
+    }
+//</editor-fold>
+    
+    
+    
+}
