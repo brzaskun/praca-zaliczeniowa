84bc5ca72f1b5160132598088b44612f00d6aefe
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2014-12-13 18:26:45 +0000

    poprawiam rozliczanie vat

--- trunk/npkpir_23/src/java/embeddable/Mce.java
+++ trunk/npkpir_23/src/java/embeddable/Mce.java
@@ -113,6 +113,20 @@ public class Mce implements Serializable{
         }
         return nowedane;
     }
+    
+    public static String[] zmniejszmiesiac(String rok, String miesiac) {
+        String[] nowedane = new String[2];
+        int mcInt = miesiacToNumber.get(miesiac);
+        if (mcInt > 1) {
+            nowedane[0] = rok;
+            nowedane[1] = numberToMiesiac.get(--mcInt);
+        } else {
+            int rokInt = Integer.parseInt(rok);
+            nowedane[0] = String.valueOf(-rokInt);
+            nowedane[1] = "12";
+        }
+        return nowedane;
+    }
     /*
     * zwraca liste uprzednich mcy
     */
