f68479343ba4879ff8b67a1e302cd712baa2e26c
Brzaskun <Brzaskun@gmail.com>
2014-12-21 21:48:21 +0000

    jestem w polowie dodawania srodka trwalego

--- npkpir_23/web/resources/js_fk/dialog_wpis_srodkitrwale.js
+++ npkpir_23/web/resources/js_fk/dialog_wpis_srodkitrwale.js
@@ -4,11 +4,4 @@
  * and open the template in the editor.
  */
 
-var ustawDateSrodekTrw = function() {
-    var dataWyst = rj("srodkiwpis:tabelasrodkitrwaleOT:0:dataprz");
-    var re = /^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/;
-    var testw = dataWyst.value;
-    if (!testw.match(re)) {
-        dataWyst.value = "b\u0142ędna data";
-    }
-};
+
