b52c4b1a05470f10aaead9a97d3abe9d6251da5a
Brzaskun <Brzaskun@gmail.com>
2015-01-22 18:41:59 +0000

    kolumny maja opis

---
+++ npkpir_23/src/java/dao/FakturaXXLKolumnaDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.FakturaXXLKolumna;
+import entity.Podatnik;
+import entityfk.Kliencifk;
+import java.io.Serializable;
+import javax.inject.Inject;
+import session.SessionFacade;
+
+/**
+ *
+ * @author Osito
+ */
+public class FakturaXXLKolumnaDAO extends DAO implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Inject private SessionFacade sessionFacade;
+
+    public FakturaXXLKolumnaDAO() {
+        super(FakturaXXLKolumna.class);
+    }
+
+    public FakturaXXLKolumnaDAO(Class entityClass) {
+        super(entityClass);
+    }
+    
+    public FakturaXXLKolumna findXXLByPodatnik(Podatnik p) {
+        return sessionFacade.findXXLByPodatnik(p);
+    }
+    
+}
