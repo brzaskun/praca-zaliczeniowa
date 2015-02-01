ef109598e691ce574c6701ba7ac4e0fb863230b8
Brzaskun <Brzaskun@gmail.com>
2015-02-01 12:45:28 +0000

    zrobilem kopiowanie planu kont z firmy do firmy

--- npkpir_23/src/java/converter/PodatConv.java
+++ npkpir_23/src/java/converter/PodatConv.java
@@ -6,51 +6,56 @@ package converter;
 
 import dao.PodatnikDAO;
 import entity.Podatnik;
+import entityfk.Konto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.ConverterException;
+import view.PodatnikView;
+import viewfk.PlanKontConverterView;
 
 /**
  *
  * @author Osito
  */
-public class PodatConv implements javax.faces.convert.Converter{
-       
+public class PodatConv implements javax.faces.convert.Converter {
+
+    private List<Podatnik> lista;
+
+    public PodatConv() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        PodatnikView podatnikView = (PodatnikView) context.getELContext().getELResolver().getValue(context.getELContext(), null, "podatnikView");
+        lista = podatnikView.getLi();
+    }
+
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
-        List<Podatnik> value;
-        PodatnikDAO podatnikDAO = new PodatnikDAO();
-        List<Podatnik> lista = new ArrayList<>();
-        if (submittedValue.trim().isEmpty()) {  
-            return null;  
-        } else {  
-            try {  
-                String number = submittedValue;  
-  
-                for (Podatnik p : lista) {  
-                    if (p.getNazwapelna() .equals(number)) {  
-                        return p;  
-                    }  
-                }  
-  
-            } catch(NumberFormatException exception) {  
-                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid klient"));  
-            }  
-        }  
-  
-        return null;  
-    }  
-  
+        if (submittedValue.trim().isEmpty()) {
+            return null;
+        } else {
+            try {
+                String number = submittedValue;
+                for (Podatnik p : lista) {
+                    if (p.getNazwapelna().equals(number)) {
+                        return p;
+                    }
+                }
+            } catch (NumberFormatException exception) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid klient"));
+            }
+        }
+        return null;
+    }
+
     @Override
-    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {  
-        if (value == null || value.equals("")) {  
-            return "";  
-        } else {  
-            return String.valueOf(((Podatnik) value).getNazwapelna());  
-        }  
-    }  
-    
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
+        if (value == null || value.equals("")) {
+            return "";
+        } else {
+            return String.valueOf(((Podatnik) value).getNazwapelna());
+        }
+    }
+
 }
