68f97b6c38a7e466e6da39a9e538835a5e549054
Brzaskun <Brzaskun@gmail.com>
2015-02-13 22:24:15 +0000

    zrobilem symulacje dla gco bez cech teraz cechy

---
+++ npkpir_23/src/java/viewfk/SymulacjaWynikuView.java
@@ -0,0 +1,293 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viewfk;
+
+import beansFK.BOFKBean;
+import beansFK.KontaFKBean;
+import dao.StronaWierszaDAO;
+import daoFK.KontoDAOfk;
+import daoFK.WierszBODAO;
+import embeddable.Mce;
+import embeddablefk.SaldoKonto;
+import entityfk.Konto;
+import entityfk.StronaWiersza;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import view.WpisView;
+import waluty.Z;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@ViewScoped
+public class SymulacjaWynikuView implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private List<SaldoKonto> listakontakoszty;
+    private List<SaldoKonto> listakontaprzychody;
+    private List<SaldoKonto> sumaSaldoKontoPrzychody;
+    private List<SaldoKonto> sumaSaldoKontoKoszty;
+    private List<PozycjeSymulacji> listapozycjisymulacji;
+    @ManagedProperty(value = "#{WpisView}")
+    private WpisView wpisView;
+    @Inject
+    private WierszBODAO wierszBODAO;
+    @Inject
+    private KontoDAOfk kontoDAOfk;
+    @Inject
+    private StronaWierszaDAO stronaWierszaDAO;
+    private List<SaldoKonto>wybraneprzychody;
+    private double sumaprzychody;
+    private List<SaldoKonto>wybranekoszty;
+    private double sumakoszty;
+
+    public SymulacjaWynikuView() {
+        sumaSaldoKontoPrzychody = new ArrayList<>();
+    }
+
+    public void init() {
+        List<Konto> kontaklienta = kontoDAOfk.findKontaOstAlitykaWynikowe(wpisView);
+        List<Konto> kontaklientaprzychody = new ArrayList<>();
+        List<Konto> kontaklientakoszty = new ArrayList<>();
+        for (Konto p : kontaklienta) {
+            if (p.getZwyklerozrachszczegolne().equals("szczególne")) {
+                kontaklientakoszty.add(p);
+                kontaklientaprzychody.add(p);
+            } else if (p.isPrzychod0koszt1()) {
+                kontaklientakoszty.add(p);
+            } else {
+                kontaklientaprzychody.add(p);
+            }
+        }
+        listakontaprzychody = przygotowanalistasald(kontaklientaprzychody, 0);
+        listakontakoszty = przygotowanalistasald(kontaklientakoszty, 1);
+        obliczsymulacje();
+    }
+
+    public void odswiezsaldoanalityczne() {
+        wpisView.wpisAktualizuj();
+        init();
+    }
+
+    private List<SaldoKonto> przygotowanalistasald(List<Konto> kontaklienta, int przychod0koszt1) {
+        List<SaldoKonto> przygotowanalista = new ArrayList<>();
+        for (Konto p : kontaklienta) {
+            SaldoKonto saldoKonto = new SaldoKonto();
+            saldoKonto.setKonto(p);
+            naniesZapisyNaKonto(saldoKonto, p);
+            saldoKonto.sumujBOZapisy();
+            saldoKonto.wyliczSaldo();
+            dodajdolisty(saldoKonto, przygotowanalista, przychod0koszt1);
+        }
+        for (int i = 1; i < przygotowanalista.size() + 1; i++) {
+            przygotowanalista.get(i - 1).setId(i);
+        }
+        sumaSaldoKontoPrzychody = new ArrayList<>();
+        sumaSaldoKontoPrzychody.add(KontaFKBean.sumujsaldakont(przygotowanalista));
+        return przygotowanalista;
+    }
+
+    private void naniesZapisyNaKonto(SaldoKonto saldoKonto, Konto p) {
+        List<StronaWiersza> zapisyRok = pobierzzapisy(p);
+        for (StronaWiersza r : zapisyRok) {
+            if (r.getWnma().equals("Wn")) {
+                saldoKonto.setObrotyWn(Z.z(saldoKonto.getObrotyWn() + r.getKwotaPLN()));
+            } else {
+                saldoKonto.setObrotyMa(Z.z(saldoKonto.getObrotyMa() + r.getKwotaPLN()));
+            }
+        }
+        saldoKonto.setZapisy(zapisyRok);
+    }
+
+    private void dodajdolisty(SaldoKonto saldoKonto, List<SaldoKonto> przygotowanalista, int przychod0koszt1) {
+        boolean kontoszczegolne = saldoKonto.getKonto().getZwyklerozrachszczegolne().equals("szczególne");
+        if (kontoszczegolne) {
+            if (saldoKonto.getSaldoWn() > 0.0 && przychod0koszt1 == 1) {
+                przygotowanalista.add(saldoKonto);
+                return;
+            }
+            if (saldoKonto.getSaldoMa() > 0.0 && przychod0koszt1 == 0) {
+                przygotowanalista.add(saldoKonto);
+                return;
+            }
+        } else {
+            if (saldoKonto.getObrotyBoWn() > 0.0 || saldoKonto.getBoWn() != 0.0) {
+                przygotowanalista.add(saldoKonto);
+                return;
+            }
+            if (saldoKonto.getObrotyBoMa() > 0.0 || saldoKonto.getBoMa() != 0.0) {
+                przygotowanalista.add(saldoKonto);
+                return;
+            }
+        }
+    }
+
+    private List<StronaWiersza> pobierzzapisy(Konto p) {
+        List<String> poprzedniemce = Mce.poprzedniemce(wpisView.getMiesiacWpisu());
+        List<StronaWiersza> zapisy = KontaFKBean.pobierzZapisyRokMc(p, wpisView.getPodatnikObiekt(), wpisView.getRokWpisuSt(), wpisView.getMiesiacWpisu(), stronaWierszaDAO);
+        for (String r : poprzedniemce) {
+            zapisy.addAll(KontaFKBean.pobierzZapisyRokMc(p, wpisView.getPodatnikObiekt(), wpisView.getRokWpisuSt(), r, stronaWierszaDAO));
+        }
+        return zapisy;
+    }
+
+    private double sumuj(List<SaldoKonto> listakonta) {
+        double suma = 0.0;
+        for (SaldoKonto p : listakonta) {
+            suma += (p.getSaldoMa() + p.getSaldoWn());
+        }
+        return suma;
+    }
+
+    private void obliczsymulacje() {
+        listapozycjisymulacji = new ArrayList<>();
+        listapozycjisymulacji.add(new PozycjeSymulacji("przychody", Z.z(sumuj(listakontaprzychody))));
+        listapozycjisymulacji.add(new PozycjeSymulacji("koszty", Z.z(sumuj(listakontakoszty))));
+        double dochod = Z.z(listapozycjisymulacji.get(0).getWartosc() - listapozycjisymulacji.get(1).getWartosc());
+        listapozycjisymulacji.add(new PozycjeSymulacji("wynik", dochod));
+        listapozycjisymulacji.add(new PozycjeSymulacji("stawka podatku", 0.19));
+        listapozycjisymulacji.add(new PozycjeSymulacji("podatek dochodowy", Z.z(dochod*.19)));
+    }
+    
+    public void sumazapisowPrzychody() {
+        sumaprzychody = 0.0;
+        for (SaldoKonto p : sumaSaldoKontoPrzychody) {
+            sumaprzychody += p.getSaldoMa();
+            sumaprzychody -= p.getSaldoWn();
+        }
+    }
+    
+    public void sumazapisowKoszty() {
+        sumakoszty = 0.0;
+        for (SaldoKonto p : sumaSaldoKontoKoszty) {
+            sumakoszty += p.getSaldoWn();
+            sumakoszty -= p.getSaldoMa();
+        }
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+
+    public List<SaldoKonto> getWybraneprzychody() {
+        return wybraneprzychody;
+    }
+
+    public void setWybraneprzychody(List<SaldoKonto> wybraneprzychody) {
+        this.wybraneprzychody = wybraneprzychody;
+    }
+
+    public double getSumaprzychody() {
+        return Z.z(sumaprzychody);
+    }
+
+    public void setSumaprzychody(double sumaprzychody) {
+        this.sumaprzychody = sumaprzychody;
+    }
+
+    public List<SaldoKonto> getWybranekoszty() {
+        return wybranekoszty;
+    }
+
+    public void setWybranekoszty(List<SaldoKonto> wybranekoszty) {
+        this.wybranekoszty = wybranekoszty;
+    }
+
+    public double getSumakoszty() {
+        return Z.z(sumakoszty);
+    }
+
+    public void setSumakoszty(double sumakoszty) {
+        this.sumakoszty = sumakoszty;
+    }
+    
+    
+    public List<PozycjeSymulacji> getListapozycjisymulacji() {
+        return listapozycjisymulacji;
+    }
+
+    public void setListapozycjisymulacji(List<PozycjeSymulacji> listapozycjisymulacji) {
+        this.listapozycjisymulacji = listapozycjisymulacji;
+    }
+
+    public List<SaldoKonto> getSumaSaldoKontoPrzychody() {
+        return sumaSaldoKontoPrzychody;
+    }
+
+    public void setSumaSaldoKontoPrzychody(List<SaldoKonto> sumaSaldoKontoPrzychody) {
+        this.sumaSaldoKontoPrzychody = sumaSaldoKontoPrzychody;
+    }
+
+    public List<SaldoKonto> getListakontakoszty() {
+        return listakontakoszty;
+    }
+
+    public void setListakontakoszty(List<SaldoKonto> listakontakoszty) {
+        this.listakontakoszty = listakontakoszty;
+    }
+
+    public WpisView getWpisView() {
+        return wpisView;
+    }
+
+    public void setWpisView(WpisView wpisView) {
+        this.wpisView = wpisView;
+    }
+
+    public List<SaldoKonto> getListakontaprzychody() {
+        return listakontaprzychody;
+    }
+
+    public void setListakontaprzychody(List<SaldoKonto> listakontaprzychody) {
+        this.listakontaprzychody = listakontaprzychody;
+    }
+
+    public List<SaldoKonto> getSumaSaldoKontoKoszty() {
+        return sumaSaldoKontoKoszty;
+    }
+
+    public void setSumaSaldoKontoKoszty(List<SaldoKonto> sumaSaldoKontoKoszty) {
+        this.sumaSaldoKontoKoszty = sumaSaldoKontoKoszty;
+    }
+
+//</editor-fold>
+    public static class PozycjeSymulacji {
+
+        private String nazwa;
+        private double wartosc;
+
+        public PozycjeSymulacji() {
+        }
+
+        public PozycjeSymulacji(String nazwa, double wartosc) {
+            this.nazwa = nazwa;
+            this.wartosc = wartosc;
+        }
+
+        public String getNazwa() {
+            return nazwa;
+        }
+
+        public void setNazwa(String nazwa) {
+            this.nazwa = nazwa;
+        }
+
+        public double getWartosc() {
+            return wartosc;
+        }
+
+        public void setWartosc(double wartosc) {
+            this.wartosc = wartosc;
+        }
+
+    }
+
+}
