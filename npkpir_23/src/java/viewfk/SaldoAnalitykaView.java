c6d4cedd52d65bd3b3e640d0d5f4729a65cdec5d
Brzaskun <Brzaskun@gmail.com>
2015-02-18 19:40:18 +0000

    naprawilem zestawienia kont i dodalem druk walut. zrobilem zestawienia syntetyczne

--- npkpir_23/src/java/viewfk/SaldoAnalitykaView.java
+++ npkpir_23/src/java/viewfk/SaldoAnalitykaView.java
@@ -18,7 +18,6 @@ import entityfk.StronaWiersza;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
