b65849043d02f9d502335c7fab0dea3231544a7a
Brzaskun <Brzaskun@gmail.com>
2015-04-12 17:45:22 +0000

    cyzelowanie rozrachunkow

---
+++ npkpir_23/src/java/viewfk/DokfkSortView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viewfk;
+
+import beansFK.DokFKBean;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@ViewScoped
+public class DokfkSortView implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public int sortZaksiegowaneDok(Object o1, Object o2) {
+        return DokFKBean.sortZaksiegowaneDok(o1, o2);
+    }
+}
