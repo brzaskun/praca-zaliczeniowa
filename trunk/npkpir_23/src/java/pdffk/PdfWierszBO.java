d2503392e6e9f8300e15422e71598cfe14560c00
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2015-05-12 12:33:18 +0000

    naprawy biurowe bilansu otwarcia

---
+++ trunk/npkpir_23/src/java/pdffk/PdfWierszBO.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pdffk;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import entity.Uz;
+import entityfk.Transakcja;
+import entityfk.WierszBO;
+import java.io.File;
+import java.util.List;
+import javax.ejb.Stateless;
+import msg.Msg;
+import org.primefaces.context.RequestContext;
+import static pdffk.PdfMain.dodajOpisWstepny;
+import static pdffk.PdfMain.dodajOpisWstepny;
+import static pdffk.PdfMain.dodajTabele;
+import static pdffk.PdfMain.finalizacjaDokumentu;
+import static pdffk.PdfMain.inicjacjaA4Portrait;
+import static pdffk.PdfMain.inicjacjaWritera;
+import static pdffk.PdfMain.naglowekStopkaP;
+import static pdffk.PdfMain.otwarcieDokumentu;
+import view.WpisView;
+
+/**
+ *
+ * @author Osito
+ */
+@Stateless
+public class PdfWierszBO {
+    public static void drukujRKK(List<WierszBO> pobranetransakcje, WpisView wpisView) {
+        String nazwa = wpisView.getPodatnikObiekt().getNip()+"wierszBO";
+        File file = new File(nazwa);
+        if (file.isFile()) {
+            file.delete();
+        }
+        if (pobranetransakcje != null && pobranetransakcje.size() > 0) {
+            Uz uz = wpisView.getWprowadzil();
+            Document document = inicjacjaA4Portrait();
+            PdfWriter writer = inicjacjaWritera(document, nazwa);
+            naglowekStopkaP(writer);
+            otwarcieDokumentu(document, nazwa);
+            dodajOpisWstepny(document, "Zestawienie wierszy BO w firmie "+wpisView.getPodatnikWpisu(), wpisView.getMiesiacWpisu(), wpisView.getRokWpisuSt());
+            dodajTabele(document, testobjects.testobjects.getTabelaWierszBO(pobranetransakcje),97,0);
+            finalizacjaDokumentu(document);
+            String f = "pokazwydruk('"+nazwa+"');";
+            RequestContext.getCurrentInstance().execute(f);
+        } else {
+            Msg.msg("w", "Nie wybrano wierszy BO do wydruku");
+        }
+    }
+}
