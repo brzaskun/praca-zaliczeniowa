7aaf2a562d25d15fa8ac827059e87bcbd1dd98ba
Brzaskun <Brzaskun@gmail.com>
2015-05-19 20:12:39 +0000

    zmiana osblugi bledu. dodano metode statyczna radzaca sobie z null

---
+++ npkpir_23/src/java/error/E.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package error;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Osito
+ */
+@Stateless
+public class E {
+ 
+    public static void e(Exception e) {
+        if (e.getStackTrace() != null && e.getStackTrace().length > 0) {
+            StringBuilder p = new StringBuilder();
+            p.append("Blad ");
+            p.append(e.getStackTrace()[0].toString());
+            p.append(" ");
+            p.append(e.toString());
+            System.out.println(p.toString());
+        } else {
+            StringBuilder p = new StringBuilder();
+            p.append("Blad ");
+            p.append(e.toString());
+            System.out.println(p.toString());
+        }
+    }
+}
