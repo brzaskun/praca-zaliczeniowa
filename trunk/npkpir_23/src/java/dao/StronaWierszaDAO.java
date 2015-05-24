23d470eedf450c1597b234e6b082d28d11e73862
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2015-05-24 09:31:19 +0000

    przyspieszania ciag dalsyz. weryfikacja pobieracnyh stron dla zestawien

--- trunk/npkpir_23/src/java/dao/StronaWierszaDAO.java
+++ trunk/npkpir_23/src/java/dao/StronaWierszaDAO.java
@@ -129,9 +129,13 @@ public class StronaWierszaDAO extends DAO implements Serializable {
     public List<StronaWiersza> findStronaByPodatnikRokWynik(Podatnik podatnik, String rok) {
         return sessionFacade.findStronaByPodatnikRokWynik(podatnik, rok);
     }
-    
+      
     public List<StronaWiersza> findStronaByPodatnikRokMcWynik(Podatnik podatnik, String rok, String mc) {
         return sessionFacade.findStronaByPodatnikRokMcWynik(podatnik, rok, mc);
+    }
+    
+    public List<StronaWiersza> findStronaByPodatnikRokMcWynikSlownik(Podatnik podatnik, String rok, String mc) {
+        return sessionFacade.findStronaByPodatnikRokMcWynikSlownik(podatnik, rok, mc);
     }
     
      public List<StronaWiersza> findStronaByPodatnikRok(Podatnik podatnik, String rok) {
