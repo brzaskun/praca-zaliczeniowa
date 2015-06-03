89f5dbcfece801961c9e05855ad625c2ca947ebf
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2015-06-03 21:00:20 +0000

    drobne poprawki

--- trunk/npkpir_23/web/resources/js_fk/dialog_wpisywanie.js
+++ trunk/npkpir_23/web/resources/js_fk/dialog_wpisywanie.js
@@ -24,7 +24,7 @@ var zachowajwierszVATRK = function (source) {
         var lp = parseInt(source)-1;
         MYAPP.lpwiersza = "formwpisdokument:dataList:"+lp+":opis";
         $(document.getElementById("wpisywaniefooter:wierszid")).val(lp);
-        $(document.getElementById("wpisywaniefooter:lpwiersza")).val(parseInt(source));
+        $(document.getElementById("wpisywaniefooter:lpwierszaRK")).val(parseInt(source));
     } catch (blad) {
         //alert("Blad w dialgowprowadzanie.js zachowaj wiersz "+blad);
     }
