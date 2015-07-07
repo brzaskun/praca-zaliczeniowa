fccc98249462cd6c27fbac0fcb0c709351fb8824
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2015-07-07 20:01:32 +0000

    poczatek wyliczania podatku kwartalnie. na razie zmienilem parametry dochodowe wyrzucajac je z podatnika. i dodajac do listy mc0kw1

---
+++ trunk/npkpir_23/src/java/dao/PodatnikOpodatkowanieDDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.PodatnikOpodatkowanieD;
+import entity.PodatnikUdzialy;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Named;
+import session.SessionFacade;
+import view.WpisView;
+
+/**
+ *
+ * @author Osito
+ */
+@Named
+public class PodatnikOpodatkowanieDDAO extends DAO implements Serializable{
+    @Inject
+    private SessionFacade sessionFacade;
+
+    public PodatnikOpodatkowanieDDAO() {
+        super(PodatnikOpodatkowanieD.class);
+    }
+
+    public List<PodatnikOpodatkowanieD> findOpodatkowaniePodatnik(WpisView wpisView) {
+        return sessionFacade.findOpodatkowaniePodatnik(wpisView);
+    }
+    
+    public PodatnikOpodatkowanieD findOpodatkowaniePodatnikRok(WpisView wpisView) {
+        return sessionFacade.findOpodatkowaniePodatnikRok(wpisView);
+    }
+    
+    
+}
