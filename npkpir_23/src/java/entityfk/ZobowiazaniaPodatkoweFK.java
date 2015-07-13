d7280d024f8dc4ae835c9aba2efd593614e387de
Brzaskun <Brzaskun@gmail.com>
2015-07-13 12:24:56 +0000

    zobowiazania podatkowe

---
+++ npkpir_23/src/java/entityfk/ZobowiazaniaPodatkoweFK.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entityfk;
+
+import entity.Podatnik;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Osito
+ */
+@Entity
+@Table(uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"podatnikObj", "rok", "mc"})})
+public class ZobowiazaniaPodatkoweFK implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "podatnikObj", referencedColumnName = "nip")
+    @ManyToOne
+    private Podatnik podatnikObj;
+    @Column(name = "rok")
+    private String rok;
+    @Column(name = "mc")
+    private String mc;
+    @Column(name = "kwota")
+    private double kwota;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Podatnik getPodatnikObj() {
+        return podatnikObj;
+    }
+
+    public void setPodatnikObj(Podatnik podatnikObj) {
+        this.podatnikObj = podatnikObj;
+    }
+
+    public String getRok() {
+        return rok;
+    }
+
+    public void setRok(String rok) {
+        this.rok = rok;
+    }
+
+    public String getMc() {
+        return mc;
+    }
+
+    public void setMc(String mc) {
+        this.mc = mc;
+    }
+
+    public double getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(double kwota) {
+        this.kwota = kwota;
+    }
+    
+    
+}
