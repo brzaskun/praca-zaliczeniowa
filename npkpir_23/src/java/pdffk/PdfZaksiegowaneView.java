6fa70f6c8b60f6cb5dc41de02f629dabb657970a
Brzaskun <Brzaskun@gmail.com>
2015-07-19 16:45:36 +0000

    drobne naprawy

--- npkpir_23/src/java/pdffk/PdfZaksiegowaneView.java
+++ npkpir_23/src/java/pdffk/PdfZaksiegowaneView.java
@@ -36,7 +36,46 @@ public class PdfZaksiegowaneView implements Serializable {
     @Inject
     private UzDAO uzDAO;
     
+    public void drukujzaksiegowanydokument(List<Dokfk> wiersze,List<Dokfk> selecteddokfk) {
+        if (wiersze != null && wiersze.size() > 0 && selecteddokfk.size() == 0) {
+            String nazwa = wpisView.getPodatnikObiekt().getNip()+"dokumentzaksiegowane";
+            File file = new File(nazwa);
+            if (file.isFile()) {
+                file.delete();
+            }
+            wydrukujzestawieniedok(nazwa, wiersze);
+        } else {
+            Msg.msg("w", "Nie wybrano wierszy do wydruku");
+        }
+        if (selecteddokfk != null && selecteddokfk.size() > 0) {
+            for (Dokfk p : selecteddokfk) {
+                String nazwa = wpisView.getPodatnikObiekt().getNip()+"dokumentzaksiegowane"+p.getDokfkPK().getNrkolejnywserii();
+                File file = new File(nazwa);
+                if (file.isFile()) {
+                    file.delete();
+                }
+                Uz uz = wpisView.getWprowadzil();
+                PdfDokfk.drukujtrescpojedynczegodok(nazwa, p, uz);
+                String f = "pokazwydruk('"+nazwa+"');";
+                RequestContext.getCurrentInstance().execute(f);
+            }
+        } else {
+            Msg.msg("w", "Nie wybrano wierszy do wydruku");
+        }
+    }
     
+    private void wydrukujzestawieniedok(String nazwa, List<Dokfk> wiersze) {
+        Uz uz = wpisView.getWprowadzil();
+        Document document = inicjacjaA4Portrait();
+        PdfWriter writer = inicjacjaWritera(document, nazwa);
+        naglowekStopkaP(writer);
+        otwarcieDokumentu(document, nazwa);
+        dodajOpisWstepny(document, "Zestawienie zaksięgowanych dokumentów", wpisView.getMiesiacWpisu(), wpisView.getRokWpisuSt());
+        dodajTabele(document, testobjects.testobjects.getTabelaZaksiegowane(wiersze), 100,0);
+        finalizacjaDokumentu(document);
+        String f = "wydrukZaksiegowaneLista('"+wpisView.getPodatnikObiekt().getNip()+"');";
+        RequestContext.getCurrentInstance().execute(f);
+    }
 
     public WpisView getWpisView() {
         return wpisView;
@@ -45,7 +84,7 @@ public class PdfZaksiegowaneView implements Serializable {
     public void setWpisView(WpisView wpisView) {
         this.wpisView = wpisView;
     }
-
-       
+   
+    
     
 }
