98585364d7d14bd0d5cfbf5525aa0eb0366f0497
Brzaskun <Brzaskun@gmail.com>
2015-07-19 21:15:16 +0000

    zrobilem wyjebiaszczy plan kont z otwieraniem i zamykaniem jak w treetable

--- npkpir_23/web/resources/js_fk/dialog_plankont.js
+++ npkpir_23/web/resources/js_fk/dialog_plankont.js
@@ -1,6 +1,6 @@
 "use strict";
-var ustalmaxlevel = function (lista) {
-    var lista = r(lista).children();
+var ustalmaxlevel = function (tabela) {
+    var lista = r(tabela).children();
     var maxlevel = 0;
     var lsize1 = lista.length;
     for (var i = 0; i < lsize1; i++) {
@@ -13,26 +13,30 @@ var ustalmaxlevel = function (lista) {
     MYAPP.currentlevel = maxlevel;
 };
 
-var zwinwszystkie = function (lista) {
-    var lista = r(lista).children();
+var zwinwszystkie = function (tabela) {
+    var lista = r(tabela).children();
     var lsize = lista.length;
     var doschowania = new Array();
+    var doklikania = new Array();
     for (var i = 0; i < lsize; i++) {
         var td = lista[i].children[0];
         var tdval = parseInt(td.innerText);
         if (tdval > 0) {
             doschowania.push(lista[i]);
-            
+        } else if (tdval === 0) {
+            doklikania.push(lista[i]);
         }
     }
     for (var key in doschowania) {
         $(doschowania[key]).hide();
     }
     MYAPP.currentlevel = 0;
+    removeczujnik(doschowania);
+    dodajczujnik(doklikania, tabela);
 };
 
-var zwinjeden = function (lista) {
-    var lista = r(lista).children();
+var zwinjeden = function (tabela) {
+    var lista = r(tabela).children();
     var currentlevel = MYAPP.currentlevel;
     if (currentlevel === 0) {
         MYAPP.currentlevel = 1;
@@ -41,47 +45,48 @@ var zwinjeden = function (lista) {
         currentlevel--;
         MYAPP.currentlevel = currentlevel;
     }
-    if (currentlevel === 0) {
-        MYAPP.currentlevel = 1;
-    }
     if (currentlevel < 0) {
         return;
     }
     var lsize = lista.length;
     var doschowania = new Array();
+    var doklikania = new Array();
     for (var i = 0; i < lsize; i++) {
         var td = lista[i].children[0];
         var tdval = parseInt(td.innerText);
         if (tdval > currentlevel) {
             doschowania.push(lista[i]);
-            
+        }
+        if (tdval === currentlevel) {
+            doklikania.push(lista[i]);
         }
     }
     for (var key in doschowania) {
         $(doschowania[key]).hide();
     }
+    removeczujnik(doschowania);
+    dodajczujnik(doklikania, tabela);
 };
 
-var rozwinwszystkie = function (lista) {
-    var lista = r(lista).children();
+var rozwinwszystkie = function (tabela) {
+    var lista = r(tabela).children();
     var lsize = lista.length;
-    var doschowania = new Array();
+    var dopokazania = new Array();
     for (var i = 0; i < lsize; i++) {
         var td = lista[i].children[0];
         var tdval = parseInt(td.innerText);
         if (tdval > 0) {
-            doschowania.push(lista[i]);
-            
+            dopokazania.push(lista[i]);
         }
     }
-    for (var key in doschowania) {
-        $(doschowania[key]).show();
+    for (var key in dopokazania) {
+        $(dopokazania[key]).show();
     }
     MYAPP.currentlevel = MYAPP.maxlevel;
 };
 
-var rozwinjeden = function (lista) {
-    var lista = r(lista).children();
+var rozwinjeden = function (tabela) {
+    var lista = r(tabela).children();
     var currentlevel = MYAPP.currentlevel;
     if (currentlevel > MYAPP.maxlevel) {
         MYAPP.currentlevel = MYAPP.maxlevel;
@@ -91,25 +96,65 @@ var rozwinjeden = function (lista) {
         MYAPP.currentlevel = currentlevel;
     }
     var lsize = lista.length;
-    var doschowania = new Array();
+    var dopokazania = new Array();
+    var doklikania = new Array();
     for (var i = 0; i < lsize; i++) {
         var td = lista[i].children[0];
         var tdval = parseInt(td.innerText);
         if (tdval < currentlevel) {
-            doschowania.push(lista[i]);
-            
+            dopokazania.push(lista[i]);
+        } else if (tdval === currentlevel) {
+            dopokazania.push(lista[i]);
+            doklikania.push(lista[i]);
         }
     }
-    for (var key in doschowania) {
-        $(doschowania[key]).show();
+    for (var key in dopokazania) {
+        $(dopokazania[key]).show();
     }
     if (currentlevel > MYAPP.maxlevel) {
         MYAPP.currentlevel = currentlevel-1;
         currentlevel--;
+        removeczujnik(dopokazania);
+    } else {
+        removeczujnik(dopokazania);
+        dodajczujnik(doklikania, tabela);
     }
 };
 
 var kopiujnazwepelnakonta = function(zrodlo,cel) {
     var nazwapelna = r(zrodlo).val();
     r(cel).val(nazwapelna);
+};
+
+var dodajczujnik = function(listawierszy,tabela) {
+    var lista = r(tabela).children();
+    for (var wiersz in listawierszy) {
+        $(listawierszy[wiersz]).dblclick(function(){
+           var ltemp0 = new Array();
+           ltemp0.push(this);
+           var nr1 = this.children[2].innerText;
+           var liczbaminusow = nr1.split('-').length;
+           for (var wiersz2 in lista) {
+               try {
+                    var nr2 = lista[wiersz2].children[2].innerText;
+                    var liczbaminusow2 = nr2.split('-').length;
+                    if (nr2.indexOf(nr1) > -1 && liczbaminusow === liczbaminusow2-1) {
+                        var ltemp = new Array();
+                        ltemp.push($(lista[wiersz2]));
+                        dodajczujnik(ltemp,tabela);
+                        $(lista[wiersz2]).show();
+                        removeczujnik(ltemp0);
+                    }
+                } catch (e) {
+                    break;
+                }
+           }
+        });
+    }
+};
+
+var removeczujnik = function(listawierszy) {
+    for (var wiersz in listawierszy) {
+        $(listawierszy[wiersz]).unbind('dblclick');
+    }
 };
