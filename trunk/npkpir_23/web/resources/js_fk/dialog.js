fe8ebb117f48ce1ec2ebe1c0bdfc49da16c18203
Brzaskun@gmail.com <Brzaskun@gmail.com@32e3eca2-c0cf-96b4-4cf9-30148aa84720>
2015-07-30 11:08:41 +0000

    naprawy biurowe

--- trunk/npkpir_23/web/resources/js_fk/dialog.js
+++ trunk/npkpir_23/web/resources/js_fk/dialog.js
@@ -37,7 +37,7 @@ var ustawdialog = function(nazwa,rodzic, szerokosc, wysokosc) {
     try {
         $(document.getElementById(nazwa)).position({
         my: "left top",
-        at: "left+40px top",
+        at: "left+20px top-40px",
         of: $(document.getElementById(rodzic)),
         collision: "none none"
         });
