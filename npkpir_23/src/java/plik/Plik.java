a5db9bd7dafe03d39d73fdf59b0f26da03c0c486
Osito <Osito@192.168.1.102>
2015-08-07 23:23:50 +0200

    plik static

---
+++ npkpir_23/src/java/plik/Plik.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plik;
+
+import java.io.File;
+
+/**
+ *
+ * @author Osito
+ */
+public class Plik {
+    public static File plik(String nazwa, boolean temp) {
+        File file = null;
+        return file;
+    }
+}
