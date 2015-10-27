b94ff57e7eb652d0f79755f3b77e2a66c7cb820f
Osito <Osito@DOM>
2015-10-27 21:30:59 +0100

    push service zrobiony :)

---
+++ npkpir_23/src/java/pushService/GrowlBean.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pushService;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.primefaces.push.EventBus;
+import org.primefaces.push.EventBusFactory;
+
+/**
+ *
+ * @author Osito
+ */
+@ManagedBean
+@RequestScoped
+public class GrowlBean {
+
+    private final static String CHANNEL = "/notify";
+    private String text, summary, detail;
+    //getters-setters
+
+    public void send() {
+        EventBus eventBus = EventBusFactory.getDefault().eventBus();
+        eventBus.publish(CHANNEL, new FacesMessage(summary, detail));
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    
+    
+}
