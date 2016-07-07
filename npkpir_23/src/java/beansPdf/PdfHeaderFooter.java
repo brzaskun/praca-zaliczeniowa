80fcaf06d9989524f373ea76d87638ef927a91b4
Osito <Osito@Dom>
2016-07-07 22:28:44 +0200

    drobne naprawybiurowe wydruki gues fk

--- npkpir_23/src/java/beansPdf/PdfHeaderFooter.java
+++ npkpir_23/src/java/beansPdf/PdfHeaderFooter.java
@@ -15,7 +15,7 @@ import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.ColumnText;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
-import javax.ejb.Stateless;
+import error.E;
 import javax.inject.Named;
 
 /**
@@ -23,43 +23,41 @@ import javax.inject.Named;
  * @author Osito
  */
 @Named
-
 public class PdfHeaderFooter extends PdfPageEventHelper {
     
         private int liczydlo = 1;
+        private Font font;
+        private Phrase header;
+        private Phrase footer;
 
         public PdfHeaderFooter() {
+            try {
+                BaseFont helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
+                this.font = new Font(helvetica,6);
+            } catch (Exception ex) {
+                E.e(ex);
+            }
         }
         
         public PdfHeaderFooter(int l) {
             this.liczydlo = l;
+            try {
+                BaseFont helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
+                this.font = new Font(helvetica,6);
+            } catch (Exception ex) {
+                E.e(ex);
+            }
         }
-        
-
-        Phrase[] header = new Phrase[2];
-        Phrase[] footer = new Phrase[2];
+       
 
         @Override
         public void onOpenDocument(PdfWriter writer, Document document) {
-            BaseFont helvetica = null;
-            try {
-                helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
-            } catch (Exception ex) {
-            }
-            Font font = new Font(helvetica,6);
-            header[0] = new Phrase("Dokument wygenerowano elektronicznie w autorskim programie księgowym Biura Rachunkowego Taxman. Nie wymaga podpisu.",font);
-            footer[0] = new Phrase(String.format("strona nr %d ", liczydlo),font);
+            header = new Phrase("Dokument wygenerowano elektronicznie w autorskim programie księgowym Biura Rachunkowego Taxman. Nie wymaga podpisu.",font);
         }
 
         @Override
         public void onStartPage(PdfWriter writer, Document document) {
-            BaseFont helvetica = null;
-            try {
-                helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
-            } catch (Exception ex) {
-            }
-            Font font = new Font(helvetica,6);
-            footer[0] = new Phrase(String.format("strona nr %d", liczydlo++),font);
+            footer = new Phrase(String.format("strona nr %d", writer.getPageNumber()),font);
         }
 
         @Override
@@ -67,23 +65,17 @@ public class PdfHeaderFooter extends PdfPageEventHelper {
             Rectangle rect = writer.getBoxSize("art");
             //dodawanie headera
             ColumnText.showTextAligned(writer.getDirectContent(),
-                    Element.ALIGN_LEFT, header[0],
+                    Element.ALIGN_LEFT, header,
                     25, rect.getBottom() - 18, 0);
             //dodawanie footera
             ColumnText.showTextAligned(writer.getDirectContent(),
-                    Element.ALIGN_CENTER, footer[0],
+                    Element.ALIGN_CENTER, footer,
                     (rect.getLeft() + rect.getRight()) / 2 , rect.getTop() + 18, 0);
         }
 
         @Override
         public void onCloseDocument(PdfWriter writer, Document document){
-            BaseFont helvetica = null;
-            try {
-                helvetica = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
-            } catch (Exception ex) {
-            }
-            Font font = new Font(helvetica,6);
-            footer[0] = new Phrase(String.format("strona nr %d - ostatnia", liczydlo),font);
+
         }
     
 }
