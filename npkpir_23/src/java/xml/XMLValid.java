a81a4e24532ba16bd1501338bcfe2bf2df754655
Osito <Osito@DESKTOP-V1MVDT8>
2019-04-28 16:45:22 +0200

    naprawy domowe faktury rozrachunki

--- npkpir_23/src/java/xml/XMLValid.java
+++ npkpir_23/src/java/xml/XMLValid.java
@@ -28,6 +28,8 @@ public class XMLValid {
     private static String schemaVATUE4 = "resources\\schema\\vatue4schemat.xsd";
     private static String schemaVATUE4l = "d:\\vatue4schemat.xsd";
     private static String schemasprfin = "d:\\schemat.xsd";
+    private static String deklaracja = "d:\\dekl.xml";
+    private static String deklaracjaschema = "d:\\schemat.xsd";
     
     
     public static Object[] walidujCMLVATUE(String deklaracja) {
@@ -155,6 +157,70 @@ public class XMLValid {
         return zwrot;
     }
     
+    public static Object[] walidujVAT7() {
+        Object[] zwrot = new Object[2];
+        zwrot[0] = Boolean.FALSE;
+        InputStream stream = null;
+        try {
+            File schemaFile = null;
+        try {
+            schemaFile = new File(deklaracjaschema);
+        } catch (Exception ex) {
+            Logger.getLogger(XMLValid.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //webapp example xsd:
+        //URL schemaFile = new URL("http://java.sun.com/xml/ns/j2ee/web-app_2_4.xsd");
+        //local file example:
+            //File schemaFile = new File(realPath); // etc.
+            String data = null;
+            System.out.println("start walidacji");
+            try {
+                FileInputStream fis = new FileInputStream(deklaracja);
+                data = IOUtils.toString(fis, "UTF-8");
+                int czyjestpodpis = data.indexOf("<Signature");
+                if (czyjestpodpis > 0) {
+                    data = data.substring(0, data.indexOf("<Signature")) + data.substring(data.indexOf("</Signature>") + 12);
+                    //System.out.println(data);
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(XMLValid.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            stream = new ByteArrayInputStream(data.getBytes("UTF-8"));
+            //Source xmlFile = new StreamSource(new File("d:\\vatue4.xml"));
+            Source xmlFile = new StreamSource(stream);
+            SchemaFactory schemaFactory = SchemaFactory
+                    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            try {
+                Schema schema = schemaFactory.newSchema(schemaFile);
+                Validator validator = schema.newValidator();
+                validator.validate(xmlFile);
+                zwrot[0] = Boolean.TRUE;
+                zwrot[1] = "Plik prawidłowy";
+                System.out.println("Plik jest prawidłowy");
+                System.out.println("Koniec walidacji bezbledna");
+            } catch (SAXException e) {
+                zwrot[0] = Boolean.FALSE;
+                zwrot[1] = obsluzblad(e);
+                System.out.println(obsluzblad(e));
+                zwrot[1] = "Błąd walidacji pliku. Brak info o szczegolach";
+            } catch (Exception e) {
+                zwrot[0] = Boolean.FALSE;
+                zwrot[1] = "Błąd walidacji pliku. Sprawdzanie przerwane";
+            }
+        } catch (Exception ex) {
+            E.e(ex);
+            System.out.println("Błąd ładowania plików do walidacji. Sprawdzanie przerwane");
+        } finally {
+            try {
+                stream.close();
+                System.out.println("byly inne bledy zamykam stream");
+            } catch (IOException ex) {
+                Logger.getLogger(XMLValid.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return zwrot;
+    }
+    
     private static String obsluzblad(SAXException e) {
         String zwrot ="Bląd walidacji: ";
         String wiadomosc = rodzajbledu(e.getMessage());
@@ -185,7 +251,7 @@ public class XMLValid {
 
     
     public static void main(String[] args) {
-        walidujsprawozdanie();
+        walidujVAT7();
     }
 
     
