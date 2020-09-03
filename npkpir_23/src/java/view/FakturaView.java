c3adfb181c4e50b24bf67180bab4ff2af2c9f370
Osito <Osito@DESKTOP-DVFG0DI>
2020-09-03 23:50:17 +0200

    naprawa faktur

--- npkpir_23/src/java/view/FakturaView.java
+++ npkpir_23/src/java/view/FakturaView.java
@@ -1200,12 +1200,14 @@ public class FakturaView implements Serializable {
         if (zapis0edycja1 == false && fakturakorekta == false) {
             String nazwaklienta = (String) Params.params("akordeon:formstworz:acForce_input");
             if (!nazwaklienta.equals("nowy klient")) {
-                if (selected.getKontrahent().getNskrocona() == null) {
-                    Msg.msg("e", "Brak nazwy skróconej kontrahenta " + selected.getKontrahent().getNpelna() + ", nie mogę poprawnie wygenerować numeru faktury. Uzupełnij dane odbiorcy faktury.");
-                    PrimeFaces.current().executeScript("PF('nazwaskroconafaktura').show();");
-                    PrimeFaces.current().executeScript("$(document.getElementById(\"formkontowybor:wybormenu\")).focus();");
-                } else {
-                    FakturaOkresowaGenNum.wygenerujnumerfaktury(fakturaDAO, selected, wpisView);
+                if (selected.getKontrahent()!=null) {
+                    if (selected.getKontrahent().getNskrocona() == null) {
+                        Msg.msg("e", "Brak nazwy skróconej kontrahenta " + selected.getKontrahent().getNpelna() + ", nie mogę poprawnie wygenerować numeru faktury. Uzupełnij dane odbiorcy faktury.");
+                        PrimeFaces.current().executeScript("PF('nazwaskroconafaktura').show();");
+                        PrimeFaces.current().executeScript("$(document.getElementById(\"formkontowybor:wybormenu\")).focus();");
+                    } else {
+                        FakturaOkresowaGenNum.wygenerujnumerfaktury(fakturaDAO, selected, wpisView);
+                    }
                 }
             }
         }
