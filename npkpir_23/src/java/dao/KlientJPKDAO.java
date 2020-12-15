252dde70acdad89264ee40af691fd4acfa4cfbb4
Osito <Osito@DESKTOP-DVFG0DI>
2020-12-15 21:40:27 +0100

    pierwsze kroki Klientjpk

---
+++ npkpir_23/src/java/dao/KlientJPKDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.KlientJPK;
+import entity.Podatnik;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Osito
+ */
+@Named
+public class KlientJPKDAO  extends DAO implements Serializable {
+    
+
+    private String test;
+    
+    public KlientJPKDAO() {
+        super(KlientJPK.class);
+    }
+    
+    public List<KlientJPK> findAll() {
+        return sessionFacade.findAll(KlientJPK.class);
+    }
+    
+    public void deleteByPodRokMc (Podatnik podatnik, String rok, String mc) {
+        sessionFacade.klientJPKdeleteByPodRokMc(podatnik, rok, mc);
+    }
+    
+    
+    
+}
