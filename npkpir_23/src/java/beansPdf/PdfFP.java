ec38f86f3104490fa3e272529f19fd4110e20fb4
Osito <Osito@DESKTOP-DVFG0DI>
2021-03-23 10:11:01 +0100

    naprawa DOMOWE

--- npkpir_23/src/java/beansPdf/PdfFP.java
+++ npkpir_23/src/java/beansPdf/PdfFP.java
@@ -251,7 +251,7 @@ public class PdfFP {
                             text = B.b("fakturakorektanr");
                         } else if (selected.isKoncowa()) {
                             text = "Faktura końcowa nr";
-                        } else if (selected.getProjektnumer()!=null && selected.getProjektnumer().length() > 0) {
+                        } else if (selected.getProjektnumer()!=null && selected.getProjektnumer().length() > 1) {
                             text = "Faktura częściowa nr";
                         } else {
                             text = B.b("fakturanr");
