208b1e0c0bc4e09d9ffcedc470c12ea7a0b358a8
Osito <Osito@DESKTOP-DVFG0DI>
2021-10-19 21:20:25 +0200

    naprawa pawlukowaksa 022020

--- npkpir_23/src/java/entity/KlientJPK.java
+++ npkpir_23/src/java/entity/KlientJPK.java
@@ -415,7 +415,7 @@ public class KlientJPK implements Serializable {
 
     @Override
     public String toString() {
-        return "KlientJPK{" + "serial=" + serial + ", kodKrajuNadania=" + kodKrajuNadania + ", kodKrajuDoreczenia=" + kodKrajuDoreczenia + ", jurysdykcja=" + jurysdykcja + ", nrKontrahenta=" + nrKontrahenta + ", nazwaKontrahenta=" + nazwaKontrahenta + ", dowodSprzedazy=" + dowodSprzedazy + ", dataWystawienia=" + dataWystawienia + ", dataSprzedazy=" + dataSprzedazy + ", netto=" + netto + ", vat=" + vat + ", nettowaluta=" + nettowaluta + ", vatwaluta=" + vatwaluta + ", stawkavat=" + stawkavat + ", rok=" + rok + ", mc=" + mc + ", ewidencja=" + ewidencja + ", ewidencjaVAT=" + ewidencjaVAT + ", waluta=" + waluta + ", wdt=" + wdt + ", wnt=" + wnt + ", eksport=" + eksport + ", importt=" + importt + '}';
+        return "KlientJPK{" + "serial=" + serial + ", kodKrajuNadania=" + kodKrajuNadania + ", kodKrajuDoreczenia=" + kodKrajuDoreczenia + ", jurysdykcja=" + jurysdykcja + ", nrKontrahenta=" + nrKontrahenta + ", nazwaKontrahenta=" + nazwaKontrahenta + ", dowodSprzedazy=" + dowodSprzedazy + ", dataWystawienia=" + dataWystawienia + ", dataSprzedazy=" + dataSprzedazy + ", netto=" + netto + ", vat=" + vat + ", nettowaluta=" + nettowaluta + ", vatwaluta=" + vatwaluta + ", stawkavat=" + stawkavat + ", rok=" + rok + ", mc=" + mc + ", ewidencja=" + ewidencja .getNazwa() + ", waluta=" + waluta + ", wdt=" + wdt + ", wnt=" + wnt + ", eksport=" + eksport + ", importt=" + importt + '}';
     }
 
     
