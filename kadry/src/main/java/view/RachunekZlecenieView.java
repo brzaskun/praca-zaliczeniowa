bf6d319394ae7ab820b0a504df1511837af0e3d9
Osito <Osito@DESKTOP-DVFG0DI>
2023-01-23 08:50:47 +0100

    naprawy domowe

--- kadry/src/main/java/view/RachunekZlecenieView.java
+++ kadry/src/main/java/view/RachunekZlecenieView.java
@@ -195,7 +195,7 @@ public class RachunekZlecenieView implements Serializable {
             }
         }
         if (zwrot!=null) {
-            rachunekdoumowyzlecenia.setKurswaluty(Z.z(zwrot.getKurssredniPrzelicznik())+1000);
+            rachunekdoumowyzlecenia.setKurswaluty(Z.z(zwrot.getKurssredniPrzelicznik()));
             rachunekdoumowyzlecenia.setDatawaluty(zwrot.getDatatabeli());
             rachunekdoumowyzlecenia.setWynagrodzeniegodzinoweoddelegowanie(Z.z(rachunekdoumowyzlecenia.getWynagrodzeniegodzinoweoddelegowaniewaluta()*zwrot.getKurssredniPrzelicznik()));
         }
