7c59dcf382b5ffc3fb88eca1e8950184ba11991f
Osito <Osito@DESKTOP-DVFG0DI>
2023-02-07 13:01:29 +0100

    nowewdsl

--- kadry/src/main/java/beanstesty/PasekwynagrodzenBean.java
+++ kadry/src/main/java/beanstesty/PasekwynagrodzenBean.java
@@ -1132,7 +1132,7 @@ public class PasekwynagrodzenBean {
     }
 
     private static void naliczzdrowota(Pasekwynagrodzen pasek, boolean nierezydent, boolean praca, String umowakodzus) {
-        double spolecznepodstawa = Z.z(pasek.getPodstawaskladkizus() + pasek.getBruttobezzus() - pasek.getRazemspolecznepracownik());
+        double spolecznepodstawa = Z.z(pasek.getPodstawaskladkizus() - pasek.getRazemspolecznepracownik());
         double podstawazdrowotna = Z.z(spolecznepodstawa + pasek.getBruttobezspolecznych()) > 0.0 ? Z.z(spolecznepodstawa + pasek.getBruttobezspolecznych()) : 0.0;
         //usuwamy z podstawy zasilki chorobowe
         List<Naliczenienieobecnosc> nieobecnoscilist = pasek.getNaliczenienieobecnoscList();
