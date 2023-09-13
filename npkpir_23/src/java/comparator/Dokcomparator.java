f41d1f10d5bdcbfa54c69b9d4c2ba662bd0f8701
Osito <Osito@PROGRAM>
2023-09-13 21:08:11 +0200

    zawiesza sie wprowadzam kolejne zmiany w plikach likwidujac pojawiajace sie bledy podczas ksiegowania

--- npkpir_23/src/java/comparator/Dokcomparator.java
+++ npkpir_23/src/java/comparator/Dokcomparator.java
@@ -22,23 +22,27 @@ public class Dokcomparator implements Comparator<Dok> {
 
     @Override
     public int compare(Dok o1, Dok o2) {
+        int zwrot = 0;
         String datao1 = o1.getDataWyst();
         String datao2 = o2.getDataWyst();
-        DateFormat formatter;
-        formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date datao1date = null;
-        Date datao2date = null;
-        try {
-             datao1date = formatter.parse(datao1);
-        } catch (ParseException ex) {
-            E.e(ex);
+        if (datao1!=null&&datao2!=null) {
+            DateFormat formatter;
+            formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date datao1date = null;
+            Date datao2date = null;
+            try {
+                 datao1date = formatter.parse(datao1);
+            } catch (ParseException ex) {
+                E.e(ex);
+            }
+            try {
+                datao2date = formatter.parse(datao2);
+            } catch (ParseException ex) {
+                E.e(ex);
+            }
+            zwrot = (datao1date.before(datao2date) ? -1 : (datao1date.equals(datao2date) ? 0 : 1));
         }
-        try {
-            datao2date = formatter.parse(datao2);
-        } catch (ParseException ex) {
-            E.e(ex);
-        }
-        return (datao1date.before(datao2date) ? -1 : (datao1date.equals(datao2date) ? 0 : 1));
+        return zwrot;
     }
     
 }
