538be1a83eb81f2276302f5c62962a3b58878a7f
brzaskun <brzaskun@gmail.com>
2023-10-29 12:01:35 +0100

    kadry usuwam webservice

--- kadry/src/main/java/webservicefaktura/FakturyKsiegowoscView.java
+++ kadry/src/main/java/webservicefaktura/FakturyKsiegowoscView.java
@@ -6,12 +6,8 @@
 package webservicefaktura;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
-import javax.xml.ws.WebServiceRef;
-import view.WpisView;
 
 /**
  *
@@ -20,27 +16,27 @@ import view.WpisView;
 @Named
 @ViewScoped
 public class FakturyKsiegowoscView implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Inject
-    private WpisView wpisView;
-    private int iloscfaktur;
-    @WebServiceRef
-    private webservicefaktura.FakturaWS_Service fakturaWS_Service;
-    
-    @PostConstruct
-    private void init() {
-//        FakturaWS fakturaWSPort = fakturaWS_Service.getFakturaWSPort();
-//        String hello = fakturaWSPort.hello("8511005008");
-//        System.out.println("pobrano "+hello);
-    }
-
-    public int getIloscfaktur() {
-        return iloscfaktur;
-    }
-
-    public void setIloscfaktur(int iloscfaktur) {
-        this.iloscfaktur = iloscfaktur;
-    }
+//    private static final long serialVersionUID = 1L;
+//    @Inject
+//    private WpisView wpisView;
+//    private int iloscfaktur;
+//    @WebServiceRef
+//    private webservicefaktura.FakturaWS_Service fakturaWS_Service;
+//    
+////    @PostConstruct
+////    private void init() {
+////        FakturaWS fakturaWSPort = fakturaWS_Service.getFakturaWSPort();
+////        String hello = fakturaWSPort.hello("8511005008");
+////        System.out.println("pobrano "+hello);
+////    }
+//
+//    public int getIloscfaktur() {
+//        return iloscfaktur;
+//    }
+//
+//    public void setIloscfaktur(int iloscfaktur) {
+//        this.iloscfaktur = iloscfaktur;
+//    }
     
     
     
