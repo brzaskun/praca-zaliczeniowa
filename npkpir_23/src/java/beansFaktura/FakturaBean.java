6a3f99ad861f3607467ce37752f4a4fc141f3306
Osito <Osito@PROGRAM>
2024-04-22 16:01:35 +0200

    napawy biurowe

--- npkpir_23/src/java/beansFaktura/FakturaBean.java
+++ npkpir_23/src/java/beansFaktura/FakturaBean.java
@@ -415,8 +415,8 @@ public class FakturaBean {
             if (selected.isLiczodwartoscibrutto()) {
                 brutto += wartosc;
                 p.setBrutto(Z.z(wartosc));
-                double podatekstawka = p.getPodatek() > -1 ? p.getPodatek() : 0;
-                podatekstawka = p.getPodatek() == -3 ? 0 : podatekstawka;
+                double podatekstawkawstepna = p.getPodatek() > -1 ? p.getPodatek() : 0;
+                double podatekstawka = p.getPodatek() == -3 ? 0 : podatekstawkawstepna;
                 double podatek = Z.z(wartosc * podatekstawka / (100+podatekstawka));
                 if (p.getPodatekkwota()!=0.0 && (podatek+0.01==p.getPodatekkwota()||podatek-0.01==p.getPodatekkwota()) ){
                     podatek = p.getPodatekkwota();
@@ -429,8 +429,8 @@ public class FakturaBean {
             } else {
                 netto += wartosc;
                 p.setNetto(wartosc);
-                double podatekstawka = p.getPodatek() > -1 ? p.getPodatek() : 0;
-                podatekstawka = p.getPodatek() == -3 ? 0 : podatekstawka;
+                double podatekstawkawstepna = p.getPodatek() > -1 ? p.getPodatek() : 0;
+                double podatekstawka = p.getPodatek() == -3 ? 0 : podatekstawkawstepna;
                 double podatek = Z.z(wartosc * podatekstawka / 100);
                 if (p.getPodatekkwota()!=0.0 && (podatek+0.01==p.getPodatekkwota()||podatek-0.01==p.getPodatekkwota()) ){
                     podatek = p.getPodatekkwota();
