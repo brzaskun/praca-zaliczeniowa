158d6bf29c62b78470b351ea00e9bfeb229dcfdb
brzaskun <brzaskun@gmail.com>
2024-08-23 16:36:58 +0200

    blad w javascript zapisy na koncie

--- npkpir_23/web/resources/js_fk/dialog_zapisynakoncie.js
+++ npkpir_23/web/resources/js_fk/dialog_zapisynakoncie.js
@@ -5,3 +5,26 @@ var zaznaczpole = function() {
     r("formwpiskonta:wyborkonta_input").select();
 };
 
+//dzieki temu toggleselect nie zaznacza juz wszystkich ignorujac filtr
+ var toggleFilteredSelect = function(checkbox, tableWidgetVar) {
+       var tableWidget = PF(tableWidgetVar);
+       var rows = tableWidget.tbody.find('tr:visible');
+       var checked = checkbox.checked;
+
+       rows.each(function() {
+           var row = $(this);
+           var checkbox = row.find('td .ui-chkbox-box');
+
+           if (checked) {
+               checkbox.addClass('ui-state-active');
+               row.addClass('ui-state-highlight');
+               row.find(':checkbox').prop('checked', true);
+           } else {
+               checkbox.removeClass('ui-state-active');
+               row.removeClass('ui-state-highlight');
+               row.find(':checkbox').prop('checked', false);
+           }
+       });
+
+       tableWidget.updateData();
+   };
