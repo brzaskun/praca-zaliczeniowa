ab565c266ec4a12f4ffb5fba19c29ca64db5158f
Osito <Osito@PROGRAM>
2024-12-10 13:13:53 +0100

    fakt

--- npkpir_23/src/java/entityfk/BankImportWzory.java
+++ npkpir_23/src/java/entityfk/BankImportWzory.java
@@ -30,8 +30,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 })
 public class BankImportWzory implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Version
-    private Long version;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
